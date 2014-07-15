d0155e49226a140344ed3021bb248e2793622947
Cojomax99 <cojomax99@gmail.com>
2014-07-15 19:12:26 +0300

    Mostly implemented fire armor

--- src/main/java/net/tropicraft/item/armor/ItemTropicraftArmor.java
+++ src/main/java/net/tropicraft/item/armor/ItemTropicraftArmor.java
@@ -1,17 +1,26 @@
 package net.tropicraft.item.armor;
 
+import java.util.List;
+
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.common.ISpecialArmor;
 import net.tropicraft.info.TCInfo;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ItemTropicraftArmor extends ItemArmor {
+public class ItemTropicraftArmor extends ItemArmor implements ISpecialArmor {
 
 	/** Name of the armor, eg "scale" or "fire", used in getArmorTexture */
 	private String modArmorName;
+	
+	@SideOnly(Side.CLIENT)
+    public static List[] fxLayers;
 	
 	public ItemTropicraftArmor(ArmorMaterial material, int renderIndex, int armorType) {
 		super(material, renderIndex, armorType);
@@ -62,5 +71,40 @@ public class ItemTropicraftArmor extends ItemArmor {
 	public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type) {
 		return getTexturePath(String.format("%s_layer_" + (slot == 2 ? 2 : 1) + ".png", modArmorName));
 	}
+
+    @Override
+    public ArmorProperties getProperties(EntityLivingBase player, ItemStack armor, DamageSource source, double damage, int slot) {
+        return new ArmorProperties(10, source == DamageSource.inFire ? 1.0 : 0.3, Integer.MAX_VALUE);
+    }
+
+    /**
+     * Get the displayed effective armor.
+     *
+     * @param player The player wearing the armor.
+     * @param armor The ItemStack of the armor item itself.
+     * @param slot The armor slot the item is in.
+     * @return The number of armor points for display, 2 per shield.
+     */
+    @Override
+    public int getArmorDisplay(EntityPlayer player, ItemStack armor, int slot) {
+        return 3;
+    }
+
+    /**
+     * Applies damage to the ItemStack. The mod is responsible for reducing the
+     * item durability and stack size. If the stack is depleted it will be cleaned
+     * up automatically.
+     *
+     * @param entity The entity wearing the armor
+     * @param armor The ItemStack of the armor item itself.
+     * @param source The source of the damage, which can be used to alter armor
+     *     properties based on the type or source of damage.
+     * @param damage The amount of damage being applied to the armor
+     * @param slot The armor slot the item is in.
+     */
+    @Override
+    public void damageArmor(EntityLivingBase entity, ItemStack stack, DamageSource source, int damage, int slot) {
+        stack.damageItem(damage, entity);        
+    }
 
 }
