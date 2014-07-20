7d55e965c2e97a0b63c806fd481a5e38d3a6f99a
TJ Jaksa <tgamer27@gmail.com>
2014-07-20 00:54:58 -0400

    Added fruit trees

--- src/main/java/net/tropicraft/world/biomes/BiomeGenTropics.java
+++ src/main/java/net/tropicraft/world/biomes/BiomeGenTropics.java
@@ -5,9 +5,12 @@ import java.util.Random;
 import net.minecraft.world.World;
 import net.tropicraft.registry.TCBlockRegistry;
 import net.tropicraft.world.worldgen.WorldGenTropicraftFlowers;
+import net.tropicraft.world.worldgen.WorldGenTropicraftFruitTrees;
 
 public class BiomeGenTropics extends BiomeGenTropicraft {
 
+	private static final int FRUIT_TREE_CHANCE = 2;
+	
 	public BiomeGenTropics(int biomeID) {
 		super(biomeID);
 	}
@@ -18,6 +21,13 @@ public class BiomeGenTropics extends BiomeGenTropicraft {
 			int i = randCoord(rand, x, 16);
 			int k = randCoord(rand, z, 16);
 			new WorldGenTropicraftFlowers(world, world.rand, TCBlockRegistry.flowers, DEFAULT_FLOWER_META).generate(i, getTerrainHeightAt(world, i, k), k);
+		}
+		
+		if(rand.nextInt(FRUIT_TREE_CHANCE) == 0) {
+			int treeType = new Random((long)(x >> 2) << 32 | (long)(z >> 2)).nextInt(4);
+			int i = randCoord(rand, x, 16);
+			int k = randCoord(rand, z, 16);
+			new WorldGenTropicraftFruitTrees(world, world.rand, treeType).generate(i, getTerrainHeightAt(world, i, k), k);
 		}
 	}
 
