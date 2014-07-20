17f3a132909a3388f1dbff09143e56ecd5fc99f0
Cojomax99 <cojomax99@gmail.com>
2014-07-20 13:14:37 +0200

    Work on sifters

--- src/main/java/net/tropicraft/block/BlockSifter.java
+++ src/main/java/net/tropicraft/block/BlockSifter.java
@@ -2,8 +2,14 @@ package net.tropicraft.block;
 
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.tropicraft.block.tileentity.TileEntitySifter;
+import net.tropicraft.registry.TCItemRegistry;
 
 public class BlockSifter extends BlockTropicraft implements ITileEntityProvider {
 
@@ -12,10 +18,44 @@ public class BlockSifter extends BlockTropicraft implements ITileEntityProvider 
     }
 
     @Override
-    public TileEntity createNewTileEntity(World var1, int var2)
-    {
-        // TODO Auto-generated method stub
-        return null;
+    public TileEntity createNewTileEntity(World var1, int var2) {
+        return new TileEntitySifter();
     }
+    
+    @Override
+    public int getRenderBlockPass() {
+        return 0;
+    }
+
+    @Override
+    public boolean isOpaqueCube() {
+        return false;
+    }
+
+    @Override
+    public boolean renderAsNormalBlock() {
+        return true;
+    }
+    
+    @Override
+    public boolean onBlockActivated(World world, int i, int j, int k, EntityPlayer entityplayer, int d, float f1, float f2, float f3) {
+        if (world.isRemote) {
+            return true;
+        }
+        
+        ItemStack stack = entityplayer.inventory.getCurrentItem();
+
+        TileEntitySifter tileentitysifta = (TileEntitySifter) world.getTileEntity(i, j, k);
+
+        if (tileentitysifta != null && stack != null && !tileentitysifta.isSifting()) {
+            Item helditem = stack.getItem();
+            if (helditem == Item.getItemFromBlock(Blocks.sand) || (helditem == TCItemRegistry.ore && stack.getItemDamage() == 5)) {
+            entityplayer.getCurrentEquippedItem().stackSize--;
+            tileentitysifta.setSifting(true);
+            }
+          //  System.out.println("setting te sifta" + tileentitysifta.sifting);
+        }
+        return true;
+    } // /o/ \o\ /o\ \o\ /o\ \o/ /o/ /o/ \o\ \o\ /o/ /o/ \o/ /o\ \o/ \o/ /o\ /o\ \o/ \o/ /o/ \o\o\o\o\o\o\o\o\o\ :D
 
 }
--- src/main/java/net/tropicraft/block/tileentity/TileEntitySifter.java
+++ src/main/java/net/tropicraft/block/tileentity/TileEntitySifter.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
@@ -11,6 +13,7 @@ import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.tropicraft.registry.TCBlockRegistry;
 
 public class TileEntitySifter extends TileEntity {
 
@@ -19,7 +22,7 @@ public class TileEntitySifter extends TileEntity {
         HEATED,
         ;
     }
-    
+
     /** Number of seconds to sift multiplied by the number of ticks per second */
     public static final int SIFT_TIME = 4 * 20;
 
@@ -33,34 +36,34 @@ public class TileEntitySifter extends TileEntity {
 
     public double yaw;
     public double yaw2 = 0.0D;
-    
+
     public ItemStack siftItem;
-    
+
     public TileEntitySifter() {
         rand = new Random();
         currentSiftTime = SIFT_TIME;
     }
-    
+
     public void updateEntity() {
         // If sifter is sifting, decrement sift time
         if(currentSiftTime > 0 && isSifting) {
             currentSiftTime--;
         }
-        
+
         // Rotation animation
         if(worldObj.isRemote) {
             this.yaw2 = this.yaw % 360.0D;
             this.yaw += 4.545454502105713D;
         }
-        
+
         // Done sifting
         if (!worldObj.isRemote && isSifting && currentSiftTime <= 0) {
             double x = this.xCoord + worldObj.rand.nextDouble()*1.4;
             double y = this.yCoord + worldObj.rand.nextDouble()*1.4;
             double z = this.zCoord + worldObj.rand.nextDouble()*1.4;
 
-           // TODO determine what to spawn
-            
+            // TODO determine what to spawn
+
             if (isHeatedSifter()) {
                 dumpResults(x, y, z, SiftType.HEATED);
             }
@@ -69,9 +72,98 @@ public class TileEntitySifter extends TileEntity {
             this.setSifting(false);
         }
     }
-    
+
     public void dumpResults(double x, double y, double z, SiftType type) {
+        float refinedAmt = getTagCompound(siftItem).getFloat("AmtRefined");
+
+        refinedAmt = refine(refinedAmt);
+
+        // If this is an unrefined ore
+        if (refinedAmt > 0) {
+            getTagCompound(siftItem).setFloat("AmtRefined", refinedAmt);
+            spawn(siftItem, x, y, z);
+        } else {
+            dumpBeachResults(x, y, z);
+        }
+    }
+
+    private void dumpBeachResults(double x, double y, double z) {
+        int dumpCount = rand.nextInt(3) + 3;
+        ItemStack stack;
         
+        while (dumpCount > 0) {
+            dumpCount--;
+            
+            if (rand.nextInt(10) == 0) {
+                stack = getRareItem();
+            } else {
+                stack = getCommonItem();
+            }
+            
+            spawn(stack, x, y, z);
+        }
+    }
+    
+    /**
+     * Spawns an EntityItem with the given ItemStack at the given coordinates
+     */
+    private void spawn(ItemStack stack, double x, double y, double z) {
+        if (worldObj.isRemote) return;
+        
+        EntityItem eitem = new EntityItem(worldObj, x, y, z, stack);
+        eitem.setLocationAndAngles(x, y, z, 0, 0);
+        worldObj.spawnEntityInWorld(eitem);
+    }
+
+    private ItemStack getCommonItem() {
+        int dmg = rand.nextInt(8);  //damage of shell
+
+        switch (dmg) {
+        case 0:
+        case 1:
+        case 2:
+        case 4:
+            //     return new ItemStack(TCItemRegistry.shells, 1, dmg);
+        default:
+            return new ItemStack(TCBlockRegistry.purifiedSand, 1);
+        }
+    }
+
+    private ItemStack getRareItem() {
+        int dmg = rand.nextInt(6);
+
+        switch (dmg) {
+        case 0:
+            //TODO  return new ItemStack(TCItemRegistry.shells, 1, 3); //rube nautilus
+        case 1:
+            return new ItemStack(Items.gold_nugget, 1);
+        case 2:
+            return new ItemStack(Items.bucket, 1);
+        case 3:
+            return new ItemStack(Items.wooden_shovel, 1);
+        case 4:
+            return new ItemStack(Items.glass_bottle, 1);
+        default:
+            //TODO       return new ItemStack(TCItemRegistry.shells, 1, 3); //rube nautilus
+
+            return new ItemStack(Items.diamond, 1);
+        }
+    }
+
+    private float refine(float refinedAmt) {
+        if (refinedAmt == 0) {
+            return 33.333F;
+        }
+
+        if (refinedAmt > 32 && refinedAmt < 34) {
+            return 66.667F;
+        }
+
+        if (refinedAmt > 65 && refinedAmt < 67) {
+            return 100.000F;
+        }
+
+        return 0F;
     }
 
     /**
@@ -80,22 +172,26 @@ public class TileEntitySifter extends TileEntity {
      */
     public boolean isHeatedSifter() {
         Block block = worldObj.getBlock(xCoord, yCoord - 1, zCoord);
-        
+
         return block.getMaterial() == Material.fire || block.getMaterial() == Material.lava;
     }
-    
+
     public void setSifting(boolean flag) {
         this.isSifting = flag;
         sync();
     }
     
+    public boolean isSifting() {
+        return this.isSifting;
+    }
+
     public void readFromNBT(NBTTagCompound nbt) {
         super.readFromNBT(nbt);
         isSifting = nbt.getBoolean("isSifting");
         currentSiftTime = nbt.getInteger("currentSiftTime");
-        
+
         NBTTagList itemtaglist = nbt.getTagList("Item", 10);
-        
+
         NBTTagCompound itemtagcompound = itemtaglist.getCompoundTagAt(0);
         this.siftItem = ItemStack.loadItemStackFromNBT(itemtagcompound);
     }
@@ -104,7 +200,7 @@ public class TileEntitySifter extends TileEntity {
         super.writeToNBT(nbt);
         nbt.setBoolean("isSifting", isSifting);
         nbt.setInteger("currentSiftTime", currentSiftTime);
-        
+
         NBTTagList nbttaglist = new NBTTagList();
 
         if (this.siftItem != null) {
@@ -112,10 +208,10 @@ public class TileEntitySifter extends TileEntity {
             this.siftItem.writeToNBT(siftItemTagCompound);
             nbttaglist.appendTag(siftItemTagCompound);
         }
-        
+
         nbt.setTag("Item", nbttaglist);
     }
-    
+
     /**
      * Retrives an existing nbt tag compound or creates a new one if it is null
      * @param stack
@@ -140,11 +236,11 @@ public class TileEntitySifter extends TileEntity {
     public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
         this.readFromNBT(pkt.func_148857_g());
     }
-    
+
     public void sync() {
         worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
     }
-    
+
     @Override
     public Packet getDescriptionPacket() {
         NBTTagCompound nbttagcompound = new NBTTagCompound();
