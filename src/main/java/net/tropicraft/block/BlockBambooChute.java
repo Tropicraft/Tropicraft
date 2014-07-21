da81e4ba707d1c64ade5f2af1c84af30e3a0d0f2
mrschlauch <Michael.Schuster.MS@gmail.com>
2014-07-21 09:05:31 +0200

    converted bamboo chute model to a simple block renderer

--- src/main/java/net/tropicraft/block/BlockBambooChute.java
+++ src/main/java/net/tropicraft/block/BlockBambooChute.java
@@ -1,11 +1,36 @@
 package net.tropicraft.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.IconFlipped;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
+import net.tropicraft.info.TCInfo;
+import net.tropicraft.info.TCNames;
 import net.tropicraft.info.TCRenderIDs;
+import net.tropicraft.registry.TCBlockRegistry;
 import net.tropicraft.registry.TCCreativeTabRegistry;
 
 public class BlockBambooChute extends BlockTropicraft {
+    @SideOnly(Side.CLIENT)
+	private IIcon sideIcon;
+    
+    @SideOnly(Side.CLIENT) 
+    private IIcon bottomIcon;
 
+    @SideOnly(Side.CLIENT) 
+    private IIcon topIcon;
+
+    @SideOnly(Side.CLIENT) 
+    private IIcon indentIcon;
+    
+    @SideOnly(Side.CLIENT)
+    private IIcon leafIcon;
+    
+    @SideOnly(Side.CLIENT)
+    private IIcon leafFlippedIcon;
+	
 	public BlockBambooChute() {
 		super(Material.plants);
 		setHardness(1.0F);
@@ -14,10 +39,50 @@ public class BlockBambooChute extends BlockTropicraft {
         this.setBlockBounds(0.5F - f, 0.0F, 0.5F - f, 0.5F + f, 1.0F, 0.5F + f);
         this.setTickRandomly(true);
         this.setCreativeTab(TCCreativeTabRegistry.tabBlock);
+        this.setBlockTextureName(TCNames.bambooChute);
 	}
 	
     @Override
+	public boolean isBlockNormalCube() {
+    	return false;
+	}
+
+	@Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
+
+	@Override
     public int getRenderType() {
         return TCRenderIDs.bambooChute;
     }
+
+	@Override
+	public void registerBlockIcons(IIconRegister iconRegister) {
+		sideIcon = iconRegister.registerIcon(TCInfo.ICON_LOCATION + this.getTextureName() + "_side");
+		topIcon = iconRegister.registerIcon(TCInfo.ICON_LOCATION + this.getTextureName() + "_top");
+		bottomIcon = iconRegister.registerIcon(TCInfo.ICON_LOCATION + this.getTextureName() + "_bottom");
+		indentIcon = iconRegister.registerIcon(TCInfo.ICON_LOCATION + this.getTextureName() + "_indent");
+		leafIcon = iconRegister.registerIcon(TCInfo.ICON_LOCATION + this.getTextureName() + "_leaf");
+		leafFlippedIcon = new IconFlipped(leafIcon, true, false);
+	}
+	
+	public static IIcon getBambooIcon(String what) {
+		if (what.equals("side")) {
+			return TCBlockRegistry.bambooChute.sideIcon;
+		} else if (what.equals("top")) {
+			return TCBlockRegistry.bambooChute.topIcon;
+		} else if (what.equals("bottom")) {
+			return TCBlockRegistry.bambooChute.bottomIcon;
+		} else if (what.equals("indent")) {
+			return TCBlockRegistry.bambooChute.indentIcon;
+		} else if (what.equals("leaf")) {
+			return TCBlockRegistry.bambooChute.leafIcon;
+		} else if (what.equals("leafFlipped")) {
+			return TCBlockRegistry.bambooChute.leafFlippedIcon;
+		} else {
+			return null;
+		}
+	}
+
 }
--- src/main/java/net/tropicraft/client/renderer/block/BambooChuteRenderHandler.java
+++ src/main/java/net/tropicraft/client/renderer/block/BambooChuteRenderHandler.java
@@ -1,22 +1,17 @@
 package net.tropicraft.client.renderer.block;
 
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
-import net.tropicraft.client.block.model.ModelBamboo;
+import net.tropicraft.block.BlockBambooChute;
 import net.tropicraft.info.TCRenderIDs;
-import net.tropicraft.util.TropicraftUtils;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
 public class BambooChuteRenderHandler implements ISimpleBlockRenderingHandler {
-
-    private ModelBamboo model;
-    
     public BambooChuteRenderHandler() {
-        model = new ModelBamboo();
     }
     
     @Override
@@ -28,12 +23,80 @@ public class BambooChuteRenderHandler implements ISimpleBlockRenderingHandler {
     public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
         
         if (modelId == getRenderId()) {
-            GL11.glPushMatrix();
-       //     Tessellator.instance.draw();
-       //     TropicraftUtils.bindTextureBlock("bamboo");
-       //     model.render(null, 0, 0, 0, 0, 0, 0.0625f);
-       //     Tessellator.instance.startDrawingQuads();
-            GL11.glPopMatrix();
+            Tessellator tessellator = Tessellator.instance;
+            tessellator.setBrightness(block.getMixedBrightnessForBlock(world, x, y, z));
+            int l = block.colorMultiplier(world, x, y, z);
+            float f = (float)(l >> 16 & 255) / 255.0F;
+            float f1 = (float)(l >> 8 & 255) / 255.0F;
+            float f2 = (float)(l & 255) / 255.0F;
+            float f4;
+
+            if (EntityRenderer.anaglyphEnable)
+            {
+                float f3 = (f * 30.0F + f1 * 59.0F + f2 * 11.0F) / 100.0F;
+                f4 = (f * 30.0F + f1 * 70.0F) / 100.0F;
+                float f5 = (f * 30.0F + f2 * 70.0F) / 100.0F;
+                f = f3;
+                f1 = f4;
+                f2 = f5;
+            }
+
+            tessellator.setColorOpaque_F(f, f1, f2);
+            
+            float sixteenth = 1f/16f;
+            IIcon sideTex = BlockBambooChute.getBambooIcon("side");
+            IIcon topTex = BlockBambooChute.getBambooIcon("top");
+            IIcon bottomTex = BlockBambooChute.getBambooIcon("bottom");
+            IIcon indentTex = BlockBambooChute.getBambooIcon("indent");
+            IIcon leafTex = BlockBambooChute.getBambooIcon("leaf");
+            IIcon leafFlippedTex = BlockBambooChute.getBambooIcon("leafFlipped");
+
+            // indent 1
+            renderer.renderFaceYNeg(block, x+sixteenth/2f, y, z+sixteenth/2f, bottomTex);
+            renderer.renderFaceXPos(block, x - 3.5*sixteenth, y, z+sixteenth/2f, indentTex);
+            renderer.renderFaceXNeg(block, x + 3.5*sixteenth, y, z+sixteenth/2f, indentTex);
+            renderer.renderFaceZPos(block, x+sixteenth/2f, y, z - 3.5*sixteenth, indentTex);
+            renderer.renderFaceZNeg(block, x+sixteenth/2f, y, z + 3.5*sixteenth, indentTex);
+
+            // bottom base
+            renderer.renderFaceYNeg(block, x, y + sixteenth, z, topTex);
+            renderer.renderFaceXPos(block, x - 3*sixteenth, y + sixteenth, z, sideTex);
+            renderer.renderFaceXNeg(block, x + 3*sixteenth, y + sixteenth, z, sideTex);
+            renderer.renderFaceZPos(block, x, y + sixteenth, z - 3*sixteenth, sideTex);
+            renderer.renderFaceZNeg(block, x, y + sixteenth, z + 3*sixteenth, sideTex);
+            renderer.renderFaceYPos(block, x, y - 8*sixteenth, z, topTex);
+            
+            // indent 2
+            renderer.renderFaceXPos(block, x - 3.5*sixteenth, y + 8*sixteenth, z+sixteenth/2f, indentTex);
+            renderer.renderFaceXNeg(block, x + 3.5*sixteenth, y + 8*sixteenth, z+sixteenth/2f, indentTex);
+            renderer.renderFaceZPos(block, x+sixteenth/2f, y + 8*sixteenth, z - 3.5*sixteenth, indentTex);
+            renderer.renderFaceZNeg(block, x+sixteenth/2f, y + 8*sixteenth, z + 3.5*sixteenth, indentTex);
+
+            // top base
+            renderer.renderFaceYNeg(block, x, y + 9*sixteenth, z, topTex);
+            renderer.renderFaceXPos(block, x - 3*sixteenth, y + 9*sixteenth, z, sideTex);
+            renderer.renderFaceXNeg(block, x + 3*sixteenth, y + 9*sixteenth, z, sideTex);
+            renderer.renderFaceZPos(block, x, y + 9*sixteenth, z - 3*sixteenth, sideTex);
+            renderer.renderFaceZNeg(block, x, y + 9*sixteenth, z + 3*sixteenth, sideTex);
+            renderer.renderFaceYPos(block, x, y, z, topTex);
+            
+            float leafOffset1 = 5.5f*sixteenth;
+            float leafOffset2 = 9f*sixteenth;
+            float leafOffsetY = 3.5f*sixteenth;
+            
+            // leaf
+            if (y % 2 == 0) {
+            	renderer.renderFaceXPos(block, x-leafOffset1, y+leafOffsetY, z+leafOffset2, leafTex);
+            	renderer.renderFaceXNeg(block, x+leafOffset1, y+leafOffsetY, z+leafOffset2, leafTex);
+            	renderer.renderFaceXPos(block, x-leafOffset1, y+leafOffsetY, z-leafOffset2, leafFlippedTex);
+            	renderer.renderFaceXNeg(block, x+leafOffset1, y+leafOffsetY, z-leafOffset2, leafFlippedTex);
+            } else {
+            	renderer.renderFaceZPos(block, x+leafOffset2, y+leafOffsetY, z-leafOffset1, leafTex);
+            	renderer.renderFaceZNeg(block, x+leafOffset2, y+leafOffsetY, z+leafOffset1, leafTex);
+            	renderer.renderFaceZPos(block, x-leafOffset2, y+leafOffsetY, z-leafOffset1, leafFlippedTex);
+            	renderer.renderFaceZNeg(block, x-leafOffset2, y+leafOffsetY, z+leafOffset1, leafFlippedTex);
+            	
+            }
         }
         
         return false;
