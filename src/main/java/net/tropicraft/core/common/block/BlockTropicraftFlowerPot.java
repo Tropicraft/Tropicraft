029966aec4c6dc3e3dd036314a2914b198dc56c9
tterrag1098 <tterrag1098@gmail.com>
2017-09-02 15:30:42 -0400

    Remove flowerpot meta<->state impl, as it was unnecessary

--- src/main/java/net/tropicraft/core/common/block/BlockTropicraftFlowerPot.java
+++ src/main/java/net/tropicraft/core/common/block/BlockTropicraftFlowerPot.java
@@ -186,13 +186,8 @@ public class BlockTropicraftFlowerPot extends BlockTropicraft implements ITileEn
 
 	@Override
 	public int getMetaFromState(IBlockState state) {
-		return ((TropicraftFlowerType) state.getValue(CONTENTS)).ordinal();
+		return 0;
 	}
-
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(CONTENTS, TropicraftFlowerType.VALUES[meta]);
-    }
     
     @Override
     public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
@@ -210,24 +205,24 @@ public class BlockTropicraftFlowerPot extends BlockTropicraft implements ITileEn
                 if (block == BlockRegistry.saplings) {
                     switch (TropicraftSaplings.byMetadata(i))
                     {
-//                        case PALM:
-//                            flowerType = TropicraftFlowerType.PALM_SAPLING;
-//                            break;
-//                        case MAHOGANY:
-//                            flowerType = TropicraftFlowerType.MAHOGANY_SAPLING;
-//                            break;
-//                        case GRAPEFRUIT:
-//                            flowerType = TropicraftFlowerType.GRAPEFRUIT_SAPLING;
-//                            break;
-//                        case LEMON:
-//                            flowerType = TropicraftFlowerType.LEMON_SAPLING;
-//                            break;
-//                        case LIME:
-//                            flowerType = TropicraftFlowerType.LIME_SAPLING;
-//                            break;
-//                        case ORANGE:
-//                            flowerType = TropicraftFlowerType.ORANGE_SAPLING;
-//                            break;
+                        case PALM:
+                            flowerType = TropicraftFlowerType.PALM_SAPLING;
+                            break;
+                        case MAHOGANY:
+                            flowerType = TropicraftFlowerType.MAHOGANY_SAPLING;
+                            break;
+                        case GRAPEFRUIT:
+                            flowerType = TropicraftFlowerType.GRAPEFRUIT_SAPLING;
+                            break;
+                        case LEMON:
+                            flowerType = TropicraftFlowerType.LEMON_SAPLING;
+                            break;
+                        case LIME:
+                            flowerType = TropicraftFlowerType.LIME_SAPLING;
+                            break;
+                        case ORANGE:
+                            flowerType = TropicraftFlowerType.ORANGE_SAPLING;
+                            break;
                         default:
                             flowerType = TropicraftFlowerType.EMPTY;
                     }
--- src/main/java/net/tropicraft/core/common/enums/TropicraftFlowerType.java
+++ src/main/java/net/tropicraft/core/common/enums/TropicraftFlowerType.java
@@ -19,15 +19,14 @@ public enum TropicraftFlowerType implements IStringSerializable {
     FERN("fern"),
     FOILAGE("foilage"),
     BROMELIAD("bromeliad"),
-//    IRIS("iris"),
-//    PINEAPPLE("pineapple"),
-//    LEMON_SAPLING("lemon_sapling"),
-//    LIME_SAPLING("lime_sapling"),
-//    GRAPEFRUIT_SAPLING("grapefruit_sapling"),
-//    ORANGE_SAPLING("orange_sapling"),
-//    PALM_SAPLING("palm_sapling"),
-//    MAHOGANY_SAPLING("mahogany_sapling"),
-    ;
+    IRIS("iris"),
+    PINEAPPLE("pineapple"),
+    LEMON_SAPLING("lemon_sapling"),
+    LIME_SAPLING("lime_sapling"),
+    GRAPEFRUIT_SAPLING("grapefruit_sapling"),
+    ORANGE_SAPLING("orange_sapling"),
+    PALM_SAPLING("palm_sapling"),
+    MAHOGANY_SAPLING("mahogany_sapling");
 
     private final String name;
     
