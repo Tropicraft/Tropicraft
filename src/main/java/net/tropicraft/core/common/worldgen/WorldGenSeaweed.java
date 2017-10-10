504749666eec3c45ca223a7e55097d8f2afad0de
tterrag <tterrag1098@gmail.com>
2017-10-10 13:29:36 -0400

    Fix possible infinite loop in seaweed gen

--- src/main/java/net/tropicraft/core/common/worldgen/WorldGenSeaweed.java
+++ src/main/java/net/tropicraft/core/common/worldgen/WorldGenSeaweed.java
@@ -2,7 +2,6 @@ package net.tropicraft.core.common.worldgen;
 
 import java.util.Random;
 
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -23,12 +22,8 @@ public class WorldGenSeaweed extends WorldGenerator {
 			for (BlockPos pos : BlockPos.getAllInBoxMutable(center.add(-8, 0, -8), center.add(8, 0, 8))) {
 				double chance = Math.pow(MathHelper.clamp((maxDistanceSq - pos.distanceSq(center)) / maxDistanceSq, 0, 1), 2);
 				if (rand.nextDouble() < chance) {
-					BlockPos toPlace = worldIn.getTopSolidOrLiquidBlock(pos);
-					IBlockState state;
-					while ((state = worldIn.getBlockState(toPlace)).getBlock() != Blocks.SAND) {
-						toPlace = toPlace.down();
-					}
-					if (state.getBlock() == Blocks.SAND && worldIn.getBlockState(toPlace.up()).getMaterial().isLiquid()) {
+					BlockPos toPlace = worldIn.getTopSolidOrLiquidBlock(pos).down();
+					if (worldIn.getBlockState(toPlace).getBlock() == Blocks.SAND && worldIn.getBlockState(toPlace.up()).getMaterial().isLiquid()) {
 						setBlockAndNotifyAdequately(worldIn, toPlace, BlockRegistry.seaweed.getDefaultState());
 						ret = true;
 					}
