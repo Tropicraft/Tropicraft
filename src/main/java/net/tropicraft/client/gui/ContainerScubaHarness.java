6140a6dd2994755c6916f2abef92398732fb1623
Cojomax99 <cojomax99@gmail.com>
2017-10-16 20:50:57 -0400

    Checkpoint: gui

--- src/main/java/net/tropicraft/client/gui/ContainerScubaHarness.java
+++ src/main/java/net/tropicraft/client/gui/ContainerScubaHarness.java
@@ -1,39 +1,123 @@
 package net.tropicraft.client.gui;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
+import net.tropicraft.core.common.item.scuba.ItemScubaChestplateGear;
+import net.tropicraft.core.common.item.scuba.ItemScubaTank;
 
 public class ContainerScubaHarness extends Container {
 
     protected final IItemHandler itemHandler;
-    
-    public ContainerScubaHarness(InventoryPlayer inventory, IItemHandler iItemHandler, EnumHand hand) {
+    private IInventory playerInventory;
+
+    public ContainerScubaHarness(InventoryPlayer playerInventory, IItemHandler iItemHandler, EnumHand hand) {
         this.itemHandler = iItemHandler;
-        
+        this.playerInventory = playerInventory;
+
         // Tank slot 1
-        this.addSlotToContainer(new SlotItemHandler(iItemHandler, 0, 0, 0));
+        this.addSlotToContainer(new SlotItemHandler(iItemHandler, 0, 67, 53) {
+            /**
+             * Check if the stack is allowed to be placed in this slot.
+             */
+            @Override
+            public boolean isItemValid(@Nullable ItemStack stack) {
+                return super.isItemValid(stack) && stack.getItem() instanceof ItemScubaTank && !this.getHasStack();
+            }
+        });
         // Tank slot 2
-        this.addSlotToContainer(new SlotItemHandler(iItemHandler, 1, 20, 0));
-        
-        for (int l = 0; l < 3; ++l) {
-            for (int j1 = 0; j1 < 9; ++j1) {
-                this.addSlotToContainer(new Slot(inventory, j1 + l * 9 + 9, 8 + j1 * 18, 103 + l * 18));
+        this.addSlotToContainer(new SlotItemHandler(iItemHandler, 1, 93, 53) {
+            /**
+             * Check if the stack is allowed to be placed in this slot.
+             */
+            @Override
+            public boolean isItemValid(@Nullable ItemStack stack) {
+                return super.isItemValid(stack) && stack.getItem() instanceof ItemScubaTank && !this.getHasStack();
+            }
+        });
+
+        for (int i1 = 0; i1 < 3; ++i1)
+        {
+            for (int k1 = 0; k1 < 9; ++k1)
+            {
+                this.addSlotToContainer(new Slot(playerInventory, k1 + i1 * 9 + 9, 8 + k1 * 18, 102 + i1 * 18 + -18));
             }
         }
-        
-        for (int i1 = 0; i1 < 9; ++i1) {
-            this.addSlotToContainer(new Slot(inventory, i1, 8 + i1 * 18, 161));
+
+        for (int j1 = 0; j1 < 9; ++j1)
+        {
+            this.addSlotToContainer(new Slot(playerInventory, j1, 8 + j1 * 18, 142));
         }
     }
 
+    /**
+     * Determines whether supplied player can use this container
+     */
     @Override
     public boolean canInteractWith(EntityPlayer playerIn) {
-        return true;
+        return true;//this.playerInventory.isUsableByPlayer(playerIn);
+    }
+
+    /**
+     * Take a stack from the specified inventory slot.
+     */
+    @Override
+    @Nullable
+    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index)
+    {
+        ItemStack itemstack = null;
+        Slot slot = (Slot)this.inventorySlots.get(index);
+
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (index < 2)
+            {
+                if (!this.mergeItemStack(itemstack1, 2, this.inventorySlots.size(), true))
+                {
+                    return null;
+                }
+            }
+            else if (this.getSlot(1).isItemValid(itemstack1) && !this.getSlot(1).getHasStack())
+            {
+                if (!this.mergeItemStack(itemstack1, 1, 2, false))
+                {
+                    return null;
+                }
+            }
+            else if (this.getSlot(0).isItemValid(itemstack1))
+            {
+                if (!this.mergeItemStack(itemstack1, 0, 1, false))
+                {
+                    return null;
+                }
+            }
+            else if (2 <= 2 || !this.mergeItemStack(itemstack1, 2, 2, false))
+            {
+                return null;
+            }
+
+            if (itemstack1.stackSize == 0)
+            {
+                slot.putStack((ItemStack)null);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+        }
+
+        return itemstack;
     }
 
 }
--- src/main/java/net/tropicraft/client/gui/GuiScubaHarness.java
+++ src/main/java/net/tropicraft/client/gui/GuiScubaHarness.java
@@ -8,7 +8,7 @@ import net.minecraft.util.ResourceLocation;
 import net.tropicraft.core.client.TropicraftRenderUtils;
 
 public class GuiScubaHarness extends GuiContainer {
-
+   
     public GuiScubaHarness(Container inventorySlotsIn) {
         super(inventorySlotsIn);
         
@@ -16,13 +16,9 @@ public class GuiScubaHarness extends GuiContainer {
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-        // Draw outer book cover //
-        float f1 = 1.35F;
-        ResourceLocation DISPENSER_GUI_TEXTURES = new ResourceLocation("textures/gui/container/dispenser.png");
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         GlStateManager.disableLighting();
-        //TropicraftRenderUtils.bindTextureGui("encyclopediaTropica");
-        this.mc.getTextureManager().bindTexture(DISPENSER_GUI_TEXTURES);
+        TropicraftRenderUtils.bindTextureGui("scubaHarness");
         drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
         
     }
