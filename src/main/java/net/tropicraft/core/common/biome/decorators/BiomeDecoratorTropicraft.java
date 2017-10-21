268eb26590d8ba00bcaabbd050fc56158c3f4294
gegy1000 <gegy1000@gmail.com>
2017-10-21 19:10:44 +0200

    Reduce biome decoration cascading chunk generation by generating coordinates in chunk center

--- src/main/java/net/tropicraft/core/common/biome/decorators/BiomeDecoratorTropicraft.java
+++ src/main/java/net/tropicraft/core/common/biome/decorators/BiomeDecoratorTropicraft.java
@@ -1,13 +1,13 @@
 package net.tropicraft.core.common.biome.decorators;
 
-import java.util.Random;
-
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeDecorator;
+
+import java.util.Random;
 
 public class BiomeDecoratorTropicraft extends BiomeDecorator {
 
@@ -39,7 +39,7 @@ public class BiomeDecoratorTropicraft extends BiomeDecorator {
 	}
 
 
-	public final int randCoord(Random rand, int base, int variance) {
-		return base + rand.nextInt(variance);
+	public final int randDecorationCoord(Random rand, int base, int variance) {
+		return base + rand.nextInt(variance) + 8;
 	}
 }
