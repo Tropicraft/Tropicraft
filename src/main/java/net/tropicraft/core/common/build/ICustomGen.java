ad7383e29fabfe1b6f78ea8614eaf43067eb5950
Corosauce <cordonfreeman@gmail.com>
2017-10-30 23:34:53 -0400

    fix bad gitignore rule

---
+++ src/main/java/net/tropicraft/core/common/build/ICustomGen.java
@@ -0,0 +1,12 @@
+package net.tropicraft.core.common.build;
+
+import net.tropicraft.core.common.build.world.BuildJob;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+public interface ICustomGen {
+    /* Method used for injecting custom generation after any pass, first pass being setting air, when complete, it sends with pass value of -1 */
+    void genPassPre(World world, BuildJob parBuildJob, int parPass);
+
+    NBTTagCompound getInitNBTTileEntity();
+}
