2f4ef300846f368763a8731d68086a908b1e38c4
tterrag <tterrag1098@gmail.com>
2017-11-13 19:49:19 -0500

    Rename CommandTropicsVillage to CommandTropicsMisc

--- src/main/java/net/tropicraft/core/common/command/CommandTropicsMisc.java
+++ src/main/java/net/tropicraft/core/common/command/CommandTropicsMisc.java
@@ -28,7 +28,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CommandTropicsVillage extends CommandBase {
+public class CommandTropicsMisc extends CommandBase {
 
     @Override
     public String getName() {
