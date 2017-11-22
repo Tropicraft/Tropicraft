6723ace1491b1811feb26ce46f90ec8e29a07e58
Cojomax99 <cojomax99@gmail.com>
2017-11-21 19:53:37 -0500

    Missing import

--- src/main/java/net/tropicraft/core/common/block/BlockTropicraftSlab.java
+++ src/main/java/net/tropicraft/core/common/block/BlockTropicraftSlab.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import javax.annotation.Nullable;
 
 import net.minecraft.block.BlockSlab;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
