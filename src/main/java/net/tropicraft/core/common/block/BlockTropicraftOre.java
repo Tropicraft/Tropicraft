015be2e287771c64b9ff059a5a136fafebd50dd7
Cojomax99 <cojomax99@gmail.com>
2018-03-27 00:49:48 -0400

    Fixes #249 - ore names on hover via HWYLA

--- src/main/java/net/tropicraft/core/common/block/BlockTropicraftOre.java
+++ src/main/java/net/tropicraft/core/common/block/BlockTropicraftOre.java
@@ -5,9 +5,12 @@ import java.util.Random;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.tropicraft.core.common.enums.TropicraftOres;
@@ -67,5 +70,16 @@ public class BlockTropicraftOre extends BlockTropicraftEnumVariants<TropicraftOr
     public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune) {
         Random rand = world instanceof World ? ((World)world).rand : new Random();
         return MathHelper.getInt(rand, 2, 7);
+    }
+    
+    /**
+     * Called when a user uses the creative pick block button on this block
+     *
+     * @param target The full target the player is looking at
+     * @return A ItemStack to add to the player's inventory, empty itemstack if nothing should be added.
+     */
+    @Override
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+    	return new ItemStack(BlockRegistry.ore, 1, getVariant(state).ordinal());
     }
 }
