005e46ea1033a34c0a084514d6633b4d705f7047
tterrag <tterrag1098@gmail.com>
2018-11-08 12:51:24 -0500

    Add model and localization for donation block

--- src/main/java/net/tropicraft/core/common/block/BlockDonation.java
+++ src/main/java/net/tropicraft/core/common/block/BlockDonation.java
@@ -1,10 +1,19 @@
 package net.tropicraft.core.common.block;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.tropicraft.core.common.block.tileentity.TileEntityFactory;
 
 public class BlockDonation extends Block implements ITileEntityProvider {
@@ -18,6 +27,13 @@ public class BlockDonation extends Block implements ITileEntityProvider {
 	@Override
 	public TileEntity createNewTileEntity(World worldIn, int meta) {
 		return TileEntityFactory.getDonationTE();
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced) {
+	    super.addInformation(stack, player, tooltip, advanced);
+	    tooltip.add(I18n.format(getUnlocalizedName() + ".tooltip"));
 	}
 
 }
