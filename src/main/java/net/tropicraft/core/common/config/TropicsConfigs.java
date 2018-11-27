6a6fd86cd644ff5bb8fe8501d8983c02e8ac8981
Paul17041993 <Paul.Hancock.17041993@live.com>
2018-11-27 15:03:37 +1100

    Revert changes other than the anim aditions

--- src/main/java/net/tropicraft/core/common/config/TropicsConfigs.java
+++ src/main/java/net/tropicraft/core/common/config/TropicsConfigs.java
@@ -24,38 +24,43 @@ public class TropicsConfigs {
     public static int factorPalmOverworld = 3;
 
     public static boolean genOverworldEIH = true;
-
     public static boolean genOverworldFlowers = true;
     public static boolean genOverworldPineapples = true;
-    public static int tallFlowerGenChanceOverworld = GenRates.TALL_FLOWERS_CHANCE;
-
     public static boolean genOverworldBamboo = true;
     public static int minBambooPerPatchOverworld = GenRates.MIN_BAMBOO;
     public static int maxBambooPerPatchOverworld = GenRates.MAX_BAMBOO;
-    public static int bambooGenChanceOverworld = GenRates.BAMBOO_CHANCE;
-
     public static int minBambooPerPatchTropics = GenRates.MIN_BAMBOO;
     public static int maxBambooPerPatchTropics = GenRates.MAX_BAMBOO;
-
+    public static boolean allowVolcanoEruption = true;
+    public static int bambooGenChanceOverworld = GenRates.BAMBOO_CHANCE;
+    public static int tallFlowerGenChanceOverworld = GenRates.TALL_FLOWERS_CHANCE;
+    
     public static boolean onlyGenFlowersInTropicalBiomes = true;
 
+    public static int tropicsOceanID = 60;
+    public static int tropicsID = 61;
+    public static int rainforestPlainsID = 62;
+    public static int rainforestHillsID = 63;
+    public static int rainforestMountainsID = 64;
+    public static int tropicsRiverID = 65;
+    public static int tropicsBeachID = 66;
+    public static int tropicsLakeID = 67;
+    public static int islandMountainsID = 68;
     public static int rainforestThicknessAmount = 2;
+
+    public static int tropicsDimensionID = -127;
 
     /* == Misc Configs == */
 
-    public static boolean allowVolcanoEruption = true;
-    public static int tropicsDimensionID = -127;
     public static String[] coconutBombWhitelist = {};
-
+    
     /* == Entity Configs == */
-
     public static boolean spawnPassiveTropicsOceanMobsOverworld = false;
     public static boolean spawnHostileTropicsOceanMobsOverworld = false;
     public static boolean spawnPassiveTropicsLandMobsOverworld = false;
     public static boolean spawnHostileTropicsLandMobsOverworld = false;
-
+    
     /* == Donations == */
-
     public static String tiltifyAppToken = "";
     public static int tiltifyCampaign = 0;
     public static int donationTrackerRefreshRate = 10;
@@ -67,7 +72,6 @@ public class TropicsConfigs {
     public static boolean disableSwimAnim = false;
     public static boolean disableSwimAnimFirstPerson = false;
 
-    /* == Config Groups == */
 
     private static final String C_GENERATION = "generation";
     private static final String C_ENTITY = "entity";
@@ -87,9 +91,6 @@ public class TropicsConfigs {
     }
 
     public static void sync() {
-
-        /* == Generation Configs == */
-
         genOverworld = config.get(C_GENERATION, "genTropicraftInOverworld", genOverworld, "If false, no Tropicraft generation will occur at all in the overworld.").getBoolean();
 
         genOverworldPalms = config.get(C_GENERATION, "genPalmsInOverworld", genOverworldPalms).getBoolean();
@@ -98,58 +99,43 @@ public class TropicsConfigs {
         factorPalmOverworld = config.get(C_GENERATION, "palmPopulationFactorInOverworld", factorPalmOverworld).getInt();
 
         genOverworldEIH = config.get(C_GENERATION, "genTropicraftEIHInOverworld", genOverworldEIH).getBoolean();
-
         genOverworldFlowers = config.get(C_GENERATION, "genTropicraftFlowersInOverworld", genOverworldFlowers).getBoolean();
         genOverworldPineapples = config.get(C_GENERATION, "genPineapplesInOverworld", genOverworldPineapples).getBoolean();
-        tallFlowerGenChanceOverworld = config.getInt("tallFlowerGenChanceOverworld", C_GENERATION, tallFlowerGenChanceOverworld, 0, 1000, "Chance of pineapple/iris spawn. Lower values = more common");
-
         genOverworldBamboo = config.get(C_GENERATION, "genBambooInOverworld", genOverworldBamboo).getBoolean();
         minBambooPerPatchOverworld = config.getInt("minBambooPerPatchOverworld", C_GENERATION, minBambooPerPatchOverworld, 0, 1000, "Minimum amount of bamboo in a patch in the overworld");
         maxBambooPerPatchOverworld = config.getInt("maxBambooPerPatchOverworld", C_GENERATION, maxBambooPerPatchOverworld, minBambooPerPatchOverworld + 1, 1001, "Maximum amount of bamboo in a patch in the overworld");
-        bambooGenChanceOverworld = config.getInt("bambooGenChanceOverworld", C_GENERATION, bambooGenChanceOverworld, 0, 1000, "Chance of bamboo spawn. Lower values = more common");
-
         minBambooPerPatchTropics = config.getInt("minBambooPerPatchTropics", C_GENERATION, minBambooPerPatchTropics, 0, 1000, "Minimum amount of bamboo in a patch in the tropics");
         maxBambooPerPatchTropics = config.getInt("maxBambooPerPatchTropics", C_GENERATION, maxBambooPerPatchTropics, minBambooPerPatchTropics + 1, 1001, "Maximum amount of bamboo in a patch in the tropics");
+        bambooGenChanceOverworld = config.getInt("bambooGenChanceOverworld", C_GENERATION, bambooGenChanceOverworld, 0, 1000, "Chance of bamboo spawn. Lower values = more common");
+        tallFlowerGenChanceOverworld = config.getInt("tallFlowerGenChanceOverworld", C_GENERATION, tallFlowerGenChanceOverworld, 0, 1000, "Chance of pineapple/iris spawn. Lower values = more common");
 
         onlyGenFlowersInTropicalBiomes = config.get(C_GENERATION, "onlyGenFlowersInTropicalBiomesOverworld", onlyGenFlowersInTropicalBiomes, "Should Tropicraft flowers only generate in tropical biomes in the overworld?").getBoolean();
-
-        rainforestThicknessAmount = config.getInt("rainforestThicknessAmount", C_GENERATION, rainforestThicknessAmount, 0, 3, "How thick should the trees in rainforest biomes be?");
-
-
-        /* == Misc Configs == */
+        
+        coconutBombWhitelist = config.get(C_MISC, "coconutBombWhitelist", coconutBombWhitelist).getStringList();
 
         allowVolcanoEruption = config.get(C_MISC, "allowVolcanoEruption", allowVolcanoEruption).getBoolean();
 
         tropicsDimensionID = config.get(C_MISC, "tropicsDimensionID", tropicsDimensionID).getInt();
 
-        coconutBombWhitelist = config.get(C_MISC, "coconutBombWhitelist", coconutBombWhitelist).getStringList();
-
-
-        /* == Entity Configs == */
+        rainforestThicknessAmount = config.getInt(C_GENERATION, "rainforestThicknessAmount", rainforestThicknessAmount, 0, 3, "How thick should the trees in rainforest biomes be?");
 
         spawnPassiveTropicsOceanMobsOverworld = config.get(C_ENTITY, "spawnPassiveTropicsOceanMobsOverworld", spawnPassiveTropicsOceanMobsOverworld, "Should Tropicraft's passive water mobs spawn in the overworld?").getBoolean();
         spawnHostileTropicsOceanMobsOverworld = config.get(C_ENTITY, "spawnHostileTropicsOceanMobsOverworld", spawnHostileTropicsOceanMobsOverworld, "Should Tropicraft's hostile water mobs spawn in the overworld?").getBoolean();
 
         spawnPassiveTropicsLandMobsOverworld = config.get(C_ENTITY, "spawnPassiveTropicsLandMobsOverworld", spawnPassiveTropicsLandMobsOverworld, "Should Tropicraft's passive land mobs spawn in the overworld?").getBoolean();
         spawnHostileTropicsLandMobsOverworld = config.get(C_ENTITY, "spawnHostileTropicsLandMobsOverworld", spawnHostileTropicsLandMobsOverworld, "Should Tropicraft's hostile land mobs spawn in the overworld?").getBoolean();
-
-
-        /* == Donations == */
-
+        
         tiltifyAppToken = config.get(C_DONATIONS, "tiltifyAppToken", tiltifyAppToken, "Add a token here to enable donation tracking, leave blank to disable").getString();
         tiltifyCampaign = config.get(C_DONATIONS, "tiltifyCampaign", tiltifyCampaign, "The tiltify campaign to track donations from").getInt();
         donationTrackerRefreshRate = config.get(C_DONATIONS, "donationTrackerRefreshRate", donationTrackerRefreshRate, "How often the tracker checks for new donations, in seconds").getInt();
         tiltifyCommandRun = config.get(C_DONATIONS, "tiltifyCommandRun", tiltifyCommandRun, "Command run when donation comes in").getString();
         donationAmountPerMonument = config.get(C_DONATIONS, "donationAmountPerMonument", donationAmountPerMonument, "Amount of $ required per monument command run").getInt();
-
+        
 
         /* == Client-side Configs == */
 
         disableSwimAnim = config.get(C_CLIENTSIDE, "disableSwimAnim", disableSwimAnim, "Set to true if swimming causes issues, such as with camera and model mods.").getBoolean();
         disableSwimAnimFirstPerson = config.get(C_CLIENTSIDE, "disableSwimAnimFirstPerson", disableSwimAnimFirstPerson, "Same as above, but only when in first-person and to yourself.").getBoolean();
-
-
-        /* == Final == */
 
         config.save();
     }
