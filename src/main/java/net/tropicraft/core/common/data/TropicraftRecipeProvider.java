ca2c8cb7887cc0fa70f214afaff5c88f4075f2d1
tterrag <tterrag1098@gmail.com>
2019-10-04 21:15:08 -0400

    Add bamboo stick, add some more recipes

--- src/main/java/net/tropicraft/core/common/data/TropicraftRecipeProvider.java
+++ src/main/java/net/tropicraft/core/common/data/TropicraftRecipeProvider.java
@@ -1,15 +1,8 @@
 package net.tropicraft.core.common.data;
 
-import static net.tropicraft.core.common.block.TropicraftBlocks.BAMBOO_BUNDLE;
-import static net.tropicraft.core.common.block.TropicraftBlocks.THATCH_BUNDLE;
-import static net.tropicraft.core.common.item.TropicraftItems.AZURITE;
-import static net.tropicraft.core.common.item.TropicraftItems.EUDIALYTE;
-import static net.tropicraft.core.common.item.TropicraftItems.MANGANESE;
-import static net.tropicraft.core.common.item.TropicraftItems.SHAKA;
-import static net.tropicraft.core.common.item.TropicraftItems.UMBRELLAS;
-import static net.tropicraft.core.common.item.TropicraftItems.ZIRCON;
+import static net.tropicraft.core.common.block.TropicraftBlocks.*;
+import static net.tropicraft.core.common.item.TropicraftItems.*;
 
-import java.util.Map.Entry;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -24,14 +17,14 @@ import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.item.DyeColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.tags.Tag;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 import net.tropicraft.core.common.TropicraftTags;
-import net.tropicraft.core.common.item.UmbrellaItem;
 
 public class TropicraftRecipeProvider extends RecipeProvider {
 
@@ -52,12 +45,38 @@ public class TropicraftRecipeProvider extends RecipeProvider {
             ShapedRecipeBuilder.shapedRecipe(UMBRELLAS.get(color).get())
                 .patternLine("WWW").patternLine(" B ").patternLine(" B ")
                 .key('W', wool)
-                .key('B', Items.BAMBOO)
+                .key('B', Tags.Items.RODS_WOODEN)
                 .addCriterion("has_" + color.getName() + "_wool", this.hasItem(wool))
                 .build(consumer);
             
             // TODO other colored items
         }
+        
+        // Override the vanilla recipe to output ours, it's tagged so it will behave the same
+        ShapedRecipeBuilder.shapedRecipe(BAMBOO_STICK.get())
+            .patternLine("X").patternLine("X")
+            .key('X', Items.BAMBOO)
+            .addCriterion("has_bamboo", this.hasItem(Items.BAMBOO))
+            .build(consumer, new ResourceLocation("stick_from_bamboo_item"));
+        
+        ShapedRecipeBuilder.shapedRecipe(BAMBOO_SPEAR.get())
+            .patternLine("X ").patternLine(" X")
+            .key('X', BAMBOO_STICK.get())
+            .addCriterion("has_bamboo_stick", this.hasItem(BAMBOO_STICK.get()))
+            .build(consumer);
+        
+        ShapelessRecipeBuilder.shapelessRecipe(RAW_COFFEE_BEAN.get())
+            .addIngredient(COFFEE_BERRY.get())
+            .addCriterion("has_coffee_bean", this.hasItem(COFFEE_BERRY.get()))
+            .build(consumer);
+        
+        food(RAW_COFFEE_BEAN, ROASTED_COFFEE_BEAN, 0.1F, consumer);
+        
+        ShapedRecipeBuilder.shapedRecipe(BAMBOO_MUG.get())
+            .patternLine("X X").patternLine("X X").patternLine("XXX")
+            .key('X', Items.BAMBOO)
+            .addCriterion("has_bamboo", this.hasItem(Items.BAMBOO))
+            .build(consumer);
 
         bundle(Blocks.BAMBOO.delegate, BAMBOO_BUNDLE, consumer);
         bundle(Blocks.SUGAR_CANE.delegate, THATCH_BUNDLE, consumer);
@@ -80,14 +99,26 @@ public class TropicraftRecipeProvider extends RecipeProvider {
     }
     
     private <T extends IItemProvider & IForgeRegistryEntry<T>> void ore(Tag<Item> source, Supplier<T> result, float xp, Consumer<IFinishedRecipe> consumer) {
-        CookingRecipeBuilder.smeltingRecipe(Ingredient.fromTag(source), result.get(), xp, 80)
+        CookingRecipeBuilder.smeltingRecipe(Ingredient.fromTag(source), result.get(), xp, 100)
             .addCriterion("has_" + safeName(source.getId()), this.hasItem(source))
             .build(consumer);
-        CookingRecipeBuilder.blastingRecipe(Ingredient.fromTag(source), result.get(), xp, 80)
+        CookingRecipeBuilder.blastingRecipe(Ingredient.fromTag(source), result.get(), xp, 100)
             .addCriterion("has_" + safeName(source.getId()), this.hasItem(source))
             .build(consumer, safeId(result.get()) + "_from_blasting");
     }
-    
+
+    private <T extends IItemProvider & IForgeRegistryEntry<T>> void food(Supplier<? extends T> source, Supplier<? extends T> result, float xp, Consumer<IFinishedRecipe> consumer) {
+        CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(source.get()), result.get(), xp, 100)
+            .addCriterion("has_" + safeName(source.get().getRegistryName()), this.hasItem(source.get()))
+            .build(consumer);
+        CookingRecipeBuilder.cookingRecipe(Ingredient.fromItems(source.get()), result.get(), xp, 100, IRecipeSerializer.SMOKING)
+            .addCriterion("has_" + safeName(source.get().getRegistryName()), this.hasItem(source.get()))
+            .build(consumer, safeId(result.get()) + "_from_smoking");
+        CookingRecipeBuilder.cookingRecipe(Ingredient.fromItems(source.get()), result.get(), xp, 100, IRecipeSerializer.CAMPFIRE_COOKING)
+            .addCriterion("has_" + safeName(source.get().getRegistryName()), this.hasItem(source.get()))
+            .build(consumer, safeId(result.get()) + "_from_campfire");
+    }
+
     private <T extends IItemProvider & IForgeRegistryEntry<T>> void bundle(Supplier<T> source, Supplier<T> result, Consumer<IFinishedRecipe> consumer) {
         ShapelessRecipeBuilder.shapelessRecipe(result.get())
             .addIngredient(source.get(), 9)
