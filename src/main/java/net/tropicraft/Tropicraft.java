f7e5c37125f04e13fda7ff399b7b960f210375c0
tterrag <tterrag1098@gmail.com>
2019-10-30 23:02:02 -0400

    Make color handler for IColoredItem items automatic

--- src/main/java/net/tropicraft/Tropicraft.java
+++ src/main/java/net/tropicraft/Tropicraft.java
@@ -1,7 +1,5 @@
 package net.tropicraft;
 
-import java.util.function.Supplier;
-
 import com.google.common.collect.ImmutableMap;
 
 import net.minecraft.block.Block;
@@ -9,6 +7,8 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.renderer.model.ModelBakery;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.BooleanProperty;
@@ -20,6 +20,7 @@ import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
@@ -91,11 +92,8 @@ import net.tropicraft.core.common.entity.placeable.WallItemEntity;
 import net.tropicraft.core.common.entity.underdasea.MarlinEntity;
 import net.tropicraft.core.common.entity.underdasea.SeahorseEntity;
 import net.tropicraft.core.common.entity.underdasea.TropicraftDolphinEntity;
-import net.tropicraft.core.common.item.BeachFloatItem;
-import net.tropicraft.core.common.item.ChairItem;
-import net.tropicraft.core.common.item.CocktailItem;
+import net.tropicraft.core.common.item.IColoredItem;
 import net.tropicraft.core.common.item.TropicraftItems;
-import net.tropicraft.core.common.item.UmbrellaItem;
 import net.tropicraft.core.common.network.TropicraftPackets;
 
 @Mod(Constants.MODID)
@@ -171,20 +169,13 @@ public class Tropicraft
     
     @OnlyIn(Dist.CLIENT)
     private void registerItemColors(ColorHandlerEvent.Item evt) {
-        for (final Supplier<UmbrellaItem> item : TropicraftItems.UMBRELLAS.values()) {
-            evt.getItemColors().register(new BasicColorHandler(), item.get());
+        BasicColorHandler basic = new BasicColorHandler();
+        for (RegistryObject<Item> ro : TropicraftItems.ITEMS.getEntries()) {
+            Item item = ro.get();
+            if (item instanceof IColoredItem) {
+                evt.getItemColors().register(basic, item);
+            }
         }
-        for (final Supplier<ChairItem> item : TropicraftItems.CHAIRS.values()) {
-            evt.getItemColors().register(new BasicColorHandler(), item.get());
-        }
-        for (final Supplier<BeachFloatItem> item : TropicraftItems.BEACH_FLOATS.values()) {
-            evt.getItemColors().register(new BasicColorHandler(), item.get());
-        }
-        for (final Supplier<CocktailItem> item : TropicraftItems.COCKTAILS.values()) {
-            evt.getItemColors().register(new BasicColorHandler(), item.get());
-        }
-
-        evt.getItemColors().register(new BasicColorHandler(), TropicraftItems.LOVE_TROPICS_SHELL::get);
     }
     
     private void setup(final FMLCommonSetupEvent event) {
