fb7ae20b592891b57ee9b7df76f328ca0f88eb23
tterrag <tterrag1098@gmail.com>
2019-10-30 22:56:29 -0400

    Clean up furniture rendering

--- src/main/java/net/tropicraft/core/client/entity/render/ChairRenderer.java
+++ src/main/java/net/tropicraft/core/client/entity/render/ChairRenderer.java
@@ -1,72 +1,20 @@
 package net.tropicraft.core.client.entity.render;
 
-import java.nio.FloatBuffer;
-
-import javax.annotation.Nullable;
-
 import com.mojang.blaze3d.platform.GlStateManager;
 
-import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
-import net.tropicraft.core.client.TropicraftRenderUtils;
 import net.tropicraft.core.client.entity.model.ChairModel;
-import net.tropicraft.core.common.ColorHelper;
 import net.tropicraft.core.common.entity.placeable.ChairEntity;
 
-public class ChairRenderer extends EntityRenderer<ChairEntity> {
-
-    final ChairModel model = new ChairModel();
-    FloatBuffer color;
-    float red = 0.0F, green = 0.0F, blue = 0.0F, alpha = 1.0F;
+public class ChairRenderer extends FurnitureRenderer<ChairEntity> {
 
     public ChairRenderer(EntityRendererManager rendererManager) {
-        super(rendererManager);
-    }
-
-    @Override
-    public void doRender(ChairEntity entity, double x, double y, double z, float yaw, float partialTicks) {
+        super(rendererManager, "chair", new ChairModel(), 0.0625f);
         this.shadowSize = 0.65f;
-        this.shadowOpaque = 1;
-        GlStateManager.pushMatrix();
-        GlStateManager.translated(x, y + 0.3125, z);
-        GlStateManager.rotatef(180F - yaw, 0.0F, 1.0F, 0.0F);
-        GlStateManager.translated(0, 0, -0.15);
-        float f2 = (float) entity.getTimeSinceHit() - partialTicks;
-        float f3 = entity.getDamage();
-        if (f3 < 0.0F) {
-            f3 = 0.0F;
-        }
-        if (f2 > 0.0F) {
-            GlStateManager.rotatef(((MathHelper.sin(f2) * f2 * f3) / 10F) * (float) entity.getForwardDirection(), 1.0F, 0.0F, 0.0F);
-        }
-
-        int color = entity.getColor().getColorValue();
-        red = ColorHelper.getRed(color);
-        green = ColorHelper.getGreen(color);
-        blue = ColorHelper.getBlue(color);
-
-        // Draw uncolored layer
-        bindTexture(TropicraftRenderUtils.getTextureEntity("chair_layer"));
-        GlStateManager.scalef(-1F, -1F, 1.0F);
-        model.render(entity, 0.0F, 1.0F, 0.1F, 0.0F, 0.0F, 0.0625F);
-
-        // Draw the colored part
-        GlStateManager.pushMatrix();
-        GlStateManager.color3f(red, green, blue);
-        bindTexture(TropicraftRenderUtils.getTextureEntity("chair_color_layer"));
-        model.render(entity, 0.0F, 1.0F, 0.1F, 0.0F, 0.0F, 0.0625F);
-        GlStateManager.disableBlend();
-        GlStateManager.color3f(1, 1, 1);
-        GlStateManager.popMatrix();
-        GlStateManager.popMatrix();
-        super.doRender(entity, x, y, z, yaw, partialTicks);
     }
-
-    @Nullable
+    
     @Override
-    protected ResourceLocation getEntityTexture(final ChairEntity chair) {
-        return null;
+    protected void setupTransforms() {
+        GlStateManager.translated(0, 0, -0.15);
     }
 }
--- src/main/java/net/tropicraft/core/client/entity/render/UmbrellaRenderer.java
+++ src/main/java/net/tropicraft/core/client/entity/render/UmbrellaRenderer.java
@@ -1,69 +1,19 @@
 package net.tropicraft.core.client.entity.render;
 
-import java.nio.FloatBuffer;
-
 import javax.annotation.Nullable;
 
-import org.lwjgl.opengl.GL11;
-
-import com.mojang.blaze3d.platform.GlStateManager;
-
-import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
-import net.tropicraft.core.client.TropicraftRenderUtils;
 import net.tropicraft.core.client.entity.model.UmbrellaModel;
-import net.tropicraft.core.common.ColorHelper;
 import net.tropicraft.core.common.entity.placeable.UmbrellaEntity;
 
-public class UmbrellaRenderer extends EntityRenderer<UmbrellaEntity> {
-
-    final UmbrellaModel model = new UmbrellaModel();
-    FloatBuffer color;
-    float red = 0.0F, green = 0.0F, blue = 0.0F, alpha = 1.0F;
+public class UmbrellaRenderer extends FurnitureRenderer<UmbrellaEntity> {
 
     public UmbrellaRenderer(EntityRendererManager rendererManager) {
-        super(rendererManager);
-    }
-
-    public void doRender(UmbrellaEntity entityUmbrella, double x, double y, double z, float yaw, float partialTicks) {
+        super(rendererManager, "umbrella", new UmbrellaModel(), 0.25f);
         this.shadowSize = 2.5f;
-        this.shadowOpaque = 1;
-        GlStateManager.pushMatrix();
-        GlStateManager.translated(x, y, z);
-        GlStateManager.rotatef(180F - yaw, 0.0F, 1.0F, 0.0F);
-        float f2 = (float) entityUmbrella.getTimeSinceHit() - partialTicks;
-        float f3 = entityUmbrella.getDamage();
-        if (f3 < 0.0F) {
-            f3 = 0.0F;
-        }
-        if (f2 > 0.0F) {
-            GlStateManager.rotatef(((MathHelper.sin(f2) * f2 * f3) / 10F) * (float) entityUmbrella.getForwardDirection(), 1.0F, 0.0F, 0.0F);
-        }
-
-        int umbrellaColor = entityUmbrella.getColor().getColorValue();
-        red = ColorHelper.getRed(umbrellaColor);
-        green = ColorHelper.getGreen(umbrellaColor);
-        blue = ColorHelper.getBlue(umbrellaColor);
-
-        // Draw arms of umbrella
-        bindTexture(TropicraftRenderUtils.getTextureEntity("umbrella_layer"));
-        GlStateManager.scalef(-1F, -1F, 1.0F);
-        model.render(entityUmbrella, 0.0F, 1.0F, 0.1F, 0.0F, 0.0F, 0.25F);
-
-        // Draw the colored part of the umbrella
-        GlStateManager.pushMatrix();
-        GlStateManager.color3f(red, green, blue);
-        bindTexture(TropicraftRenderUtils.getTextureEntity("umbrella_color_layer"));
-        model.render(entityUmbrella, 0.0F, 1.0F, 0.1F, 0.0F, 0.0F, 0.25F);
-        GlStateManager.disableBlend();
-        GlStateManager.color3f(1, 1, 1);
-        GlStateManager.popMatrix();
-        GlStateManager.popMatrix();
-        super.doRender(entityUmbrella, x, y, z, yaw, partialTicks);
     }
     
     @Override
@@ -72,17 +22,14 @@ public class UmbrellaRenderer extends EntityRenderer<UmbrellaEntity> {
             if (this.renderManager.options.entityShadows && this.shadowSize > 0.0F && !entityIn.isInvisible() && this.renderManager.isRenderShadow()) {
                 // Don't do distance culling for umbrella shadows
                 UmbrellaEntity entityUmbrella = (UmbrellaEntity) entityIn;
-                float f2 = (float) entityUmbrella.getTimeSinceHit() - partialTicks;
-                float f3 = entityUmbrella.getDamage();
-                if (f3 < 0.0F) {
-                    f3 = 0.0F;
-                }
+ 
                 // Calculate which direction the umbrella is "shaking"
                 Vec3d offsetVec = Vec3d.ZERO;
-                if (f2 > 0.0F) {
-                    float offset = ((MathHelper.sin(f2) * f2 * f3) / 100F) * (float) entityUmbrella.getForwardDirection();
+                float offset = getRockingAngle(entityUmbrella, partialTicks) / 10f;
+                if (offset != 0.0F) {
                     offsetVec = offsetVec.add(offset, 0, 0).rotateYaw((float) Math.toRadians(90 - yaw));
                 }
+                
                 // Move around the shadow renderer based on the shake
                 entityIn.lastTickPosX += offsetVec.x;
                 entityIn.posX += offsetVec.x;
