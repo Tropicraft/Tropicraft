cea826a61a8d0bc62903c8e470d9c5ff6f71f7a3
kingbdogz <brandon@gildedgames.com>
2019-11-11 21:01:13 +0800

    Check for server player not normal

--- src/main/java/net/tropicraft/core/common/minigames/MinigameManager.java
+++ src/main/java/net/tropicraft/core/common/minigames/MinigameManager.java
@@ -481,6 +481,6 @@ public class MinigameManager implements IMinigameManager
     }
 
     private boolean isInInstance(Entity entity) {
-        return entity instanceof PlayerEntity && this.currentInstance != null && this.currentInstance.getAllPlayerUUIDs().contains(entity.getUniqueID());
+        return entity instanceof ServerPlayerEntity && this.currentInstance != null && this.currentInstance.getAllPlayerUUIDs().contains(entity.getUniqueID());
     }
 }
