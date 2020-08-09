b2e259f4f520e1269b0558bdde13017531a52992
tterrag <tterrag1098@gmail.com>
2020-08-09 12:15:41 -0400

    Make coconuts break if they lose their support block

--- src/main/java/net/tropicraft/core/common/block/CoconutBlock.java
+++ src/main/java/net/tropicraft/core/common/block/CoconutBlock.java
@@ -2,14 +2,19 @@ package net.tropicraft.core.common.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.DirectionalBlock;
+import net.minecraft.block.HorizontalFaceBlock;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
 
 public class CoconutBlock extends DirectionalBlock {
     private static final VoxelShape COCONUT_AABB = Block.makeCuboidShape(4, 0.0D, 4, 12, 10, 12);
@@ -22,7 +27,20 @@ public class CoconutBlock extends DirectionalBlock {
     protected void fillStateContainer(Builder<Block, BlockState> builder) {
         super.fillStateContainer(builder);
         builder.add(FACING);
-    }
+	}
+
+	@Override
+	public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+		return HorizontalFaceBlock.func_220185_b(worldIn, pos, state.get(FACING));
+	}
+
+	@Override
+	@Deprecated
+	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+		return stateIn.get(FACING) == facing && !stateIn.isValidPosition(worldIn, currentPos)
+				? Blocks.AIR.getDefaultState()
+				: super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+	}
 
     @Override
     public VoxelShape getShape(final BlockState state, final IBlockReader worldIn, final BlockPos pos, final ISelectionContext context) {
