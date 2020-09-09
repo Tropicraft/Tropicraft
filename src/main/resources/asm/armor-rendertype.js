567b568e3476c872c7a5f9c0fd658c26c099ad73
tterrag <tterrag1098@gmail.com>
2020-09-09 02:01:07 -0400

    Fix scuba gear rendering crashing outside of dev

--- src/main/resources/asm/armor-rendertype.js
+++ src/main/resources/asm/armor-rendertype.js
@@ -18,12 +18,16 @@ function initializeCoreMod() {
                 // Add model to the bottom of the stack for instance method call
                 method.instructions.insert(firstAload, new VarInsnNode(Opcodes.ALOAD, 5));
                 
+                // Map method names
+                var getEntityCutoutNoCull = ASM.mapMethod("func_228640_c_");
+                var getRenderType = ASM.mapMethod("func_228282_a_");
+                
                 // Find and replace static method call with call to model.getRenderType
-                var methodCall = ASM.findFirstMethodCall(method, ASM.MethodType.STATIC, 'net/minecraft/client/renderer/RenderType', 'getEntityCutoutNoCull', '(Lnet/minecraft/util/ResourceLocation;)Lnet/minecraft/client/renderer/RenderType;');
+                var methodCall = ASM.findFirstMethodCall(method, ASM.MethodType.STATIC, 'net/minecraft/client/renderer/RenderType', getEntityCutoutNoCull, '(Lnet/minecraft/util/ResourceLocation;)Lnet/minecraft/client/renderer/RenderType;');
                 if (!methodCall) {
                     throw 'Could not find method call';
                 }
-                method.instructions.set(methodCall, ASM.buildMethodCall('net/minecraft/client/renderer/entity/model/BipedModel', 'getRenderType', '(Lnet/minecraft/util/ResourceLocation;)Lnet/minecraft/client/renderer/RenderType;', ASM.MethodType.VIRTUAL));
+                method.instructions.set(methodCall, ASM.buildMethodCall('net/minecraft/client/renderer/entity/model/BipedModel', getRenderType, '(Lnet/minecraft/util/ResourceLocation;)Lnet/minecraft/client/renderer/RenderType;', ASM.MethodType.VIRTUAL));
                 return method;
             }
         }
