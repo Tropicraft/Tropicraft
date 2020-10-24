2cb1a2c317cb9343f45731e1b5c7f599cc419851
tterrag <tterrag1098@gmail.com>
2020-10-24 15:13:53 -0400

    Fix flower block selection boxes not being offset

--- src/main/java/net/tropicraft/core/common/block/TropicsFlowerBlock.java
+++ src/main/java/net/tropicraft/core/common/block/TropicsFlowerBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.FlowerBlock;
 import net.minecraft.potion.Effect;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
@@ -17,9 +18,10 @@ public class TropicsFlowerBlock extends FlowerBlock {
         super(effect, effectDuration, properties);
         this.shape = shape;
     }
-    
-    @Override
-    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return shape;
-    }
+
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		Vec3d vec3d = state.getOffset(worldIn, pos);
+		return shape.withOffset(vec3d.x, vec3d.y, vec3d.z);
+	}
 }
