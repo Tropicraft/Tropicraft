08bd0a3a3eea7bf850a6c0c6a903b95e27dbd0f4
Gegy <gegy1000@gmail.com>
2021-04-25 14:23:11 +0200

    upgrade 1.15 worlds, support retroactively adding tropics dimension, match 1.15 noise

--- src/main/java/net/tropicraft/core/common/dimension/TropicraftDimension.java
+++ src/main/java/net/tropicraft/core/common/dimension/TropicraftDimension.java
@@ -1,6 +1,7 @@
 package net.tropicraft.core.common.dimension;
 
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
@@ -14,19 +15,28 @@ import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.DimensionSettings;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.FolderName;
+import net.minecraft.world.storage.SaveFormat;
 import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.hooks.BasicEventHooks;
 import net.tropicraft.Constants;
 import net.tropicraft.core.common.dimension.biome.TropicraftBiomeProvider;
 import net.tropicraft.core.common.dimension.chunk.TropicraftChunkGenerator;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.LinkedHashSet;
 import java.util.function.Supplier;
 
+@Mod.EventBusSubscriber(modid = Constants.MODID)
 public class TropicraftDimension {
     private static final Logger LOGGER = LogManager.getLogger(TropicraftDimension.class);
 
@@ -37,6 +47,33 @@ public class TropicraftDimension {
     public static final RegistryKey<DimensionType> DIMENSION_TYPE = RegistryKey.getOrCreateKey(Registry.DIMENSION_TYPE_KEY, ID);
     public static final RegistryKey<DimensionSettings> DIMENSION_SETTINGS = RegistryKey.getOrCreateKey(Registry.NOISE_SETTINGS_KEY, ID);
 
+    @SubscribeEvent
+    public static void onWorldLoad(WorldEvent.Load event) {
+        if (event.getWorld() instanceof ServerWorld) {
+            ServerWorld world = (ServerWorld) event.getWorld();
+            if (world.getDimensionKey() == World.OVERWORLD) {
+                upgradeTropicraftDimension(world.getServer());
+            }
+        }
+    }
+
+    private static void upgradeTropicraftDimension(MinecraftServer server) {
+        // forge put dimensions in a different place to where vanilla does with its custom dimension support
+        // we need to move our old data to the correct place if it exists
+
+        SaveFormat.LevelSave save = server.anvilConverterForAnvilFile;
+
+        File oldDimension = save.resolveFilePath(new FolderName("tropicraft/tropics")).toFile();
+        File newDimension = save.getDimensionFolder(WORLD);
+        if (oldDimension.exists() && !newDimension.exists()) {
+            try {
+                FileUtils.moveDirectory(oldDimension, newDimension);
+            } catch (IOException e) {
+                LOGGER.error("Failed to move old tropicraft dimension to new location!", e);
+            }
+        }
+    }
+
     @SuppressWarnings("unchecked")
     public static void addDefaultDimensionKey() {
         try {
@@ -46,6 +83,18 @@ public class TropicraftDimension {
         } catch (ReflectiveOperationException e) {
             LOGGER.error("Failed to add tropics as a default dimension key", e);
         }
+    }
+
+    public static Dimension createDimension(
+            Registry<DimensionType> dimensionTypeRegistry,
+            Registry<Biome> biomeRegistry,
+            Registry<DimensionSettings> dimensionSettingsRegistry,
+            long seed
+    ) {
+        Supplier<DimensionType> dimensionType = () -> dimensionTypeRegistry.getOrThrow(TropicraftDimension.DIMENSION_TYPE);
+        ChunkGenerator generator = TropicraftDimension.createGenerator(biomeRegistry, dimensionSettingsRegistry, seed);
+
+        return new Dimension(dimensionType, generator);
     }
 
     public static ChunkGenerator createGenerator(Registry<Biome> biomeRegistry, Registry<DimensionSettings> dimensionSettingsRegistry, long seed) {
