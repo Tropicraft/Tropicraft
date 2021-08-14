6af623438c77c208e107d5aaca58dd6038bd7021
Gegy <gegy1000@gmail.com>
2021-08-14 22:50:20 +0200

    add small muddy streams to mangroves
    Co-authored-by: SuperCoder7979 <25208576+supercoder7979@users.noreply.github.com>

--- src/main/java/net/tropicraft/core/common/dimension/surfacebuilders/MangroveSurfaceBuilder.java
+++ src/main/java/net/tropicraft/core/common/dimension/surfacebuilders/MangroveSurfaceBuilder.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.SharedSeedRandom;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.gen.PerlinNoiseGenerator;
@@ -17,7 +18,8 @@ import java.util.stream.IntStream;
 public class MangroveSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderConfig> {
     private static final SurfaceBuilderConfig MUD = new SurfaceBuilderConfig(TropicraftBlocks.MUD.get().getDefaultState(), Blocks.DIRT.getDefaultState(), TropicraftBlocks.MUD.get().getDefaultState());
 
-    private PerlinNoiseGenerator noise;
+    private PerlinNoiseGenerator mudNoise;
+    private PerlinNoiseGenerator streamNoise;
     private long seed;
 
     public MangroveSurfaceBuilder(Codec<SurfaceBuilderConfig> codec) {
@@ -26,15 +28,45 @@ public class MangroveSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderConfig>
 
     @Override
     public void buildSurface(Random random, IChunk chunk, Biome biome, int x, int z, int startHeight, double noise, BlockState defaultBlock, BlockState defaultFluid, int seaLevel, long seed, SurfaceBuilderConfig config) {
-        double mudNoise = this.noise.noiseAt(x * 0.03125, z * 0.03125, false);
-        SurfaceBuilder.DEFAULT.buildSurface(random, chunk, biome, x, z, startHeight, noise, defaultBlock, defaultFluid, seaLevel, seed, mudNoise > 0.25 ? MUD : config);
+        double streamNoise = this.streamNoise.noiseAt(x * 0.025, z * 0.025, false);
+        double mudNoise = this.mudNoise.noiseAt(x * 0.03125, z * 0.03125, false);
+        boolean placeMud = mudNoise > 0.25;
+
+        if (streamNoise > -0.1 && streamNoise < 0.1) {
+            this.placeStream(chunk, x, z, startHeight, defaultFluid, seaLevel);
+        }
+
+        if (streamNoise > -0.2 && streamNoise < 0.2) {
+            double chance = 1 - (Math.abs(streamNoise) * 5);
+            placeMud = random.nextDouble() > chance;
+        }
+
+
+        SurfaceBuilder.DEFAULT.buildSurface(random, chunk, biome, x, z, startHeight, noise, defaultBlock, defaultFluid, seaLevel, seed, placeMud ? MUD : config);
+    }
+
+    private void placeStream(IChunk chunk, int x, int z, int startHeight, BlockState defaultFluid, int seaLevel) {
+        int localX = x & 15;
+        int localZ = z & 15;
+
+        BlockPos.Mutable mutablePos = new BlockPos.Mutable();
+        for (int y = startHeight; y >= 0; y--) {
+            mutablePos.setPos(localX, y, localZ);
+            if (!chunk.getBlockState(mutablePos).isAir()) {
+                if (y + 1 == seaLevel && !chunk.getBlockState(mutablePos).matchesBlock(defaultFluid.getBlock())) {
+                    chunk.setBlockState(mutablePos, defaultFluid, false);
+                }
+                break;
+            }
+        }
     }
 
     @Override
     public void setSeed(long seed) {
-        if (this.seed != seed || this.noise == null) {
+        if (this.seed != seed || this.mudNoise == null) {
             SharedSeedRandom random = new SharedSeedRandom(seed);
-            this.noise = new PerlinNoiseGenerator(random, IntStream.rangeClosed(0, 2));
+            this.mudNoise = new PerlinNoiseGenerator(random, IntStream.rangeClosed(0, 2));
+            this.streamNoise = new PerlinNoiseGenerator(random, IntStream.rangeClosed(0, 2));
         }
         this.seed = seed;
     }
