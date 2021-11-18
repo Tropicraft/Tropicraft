4dcc22eb9d6f1b058a45d133f370cbc4b20c0906
Gegy <gegy1000@gmail.com>
2021-11-18 08:58:40 +0200

    simplify and clarify home tree branch grower logic

--- src/main/java/net/tropicraft/core/common/dimension/feature/HomeTreeBranchFeature.java
+++ src/main/java/net/tropicraft/core/common/dimension/feature/HomeTreeBranchFeature.java
@@ -2,9 +2,10 @@ package net.tropicraft.core.common.dimension.feature;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.block.BlockState;
+import net.minecraft.util.Direction;
 import net.minecraft.util.SharedSeedRandom;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3i;
 import net.minecraft.world.ISeedReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.gen.ChunkGenerator;
@@ -19,10 +20,8 @@ import net.tropicraft.core.common.dimension.feature.tree.RainforestVinesFeature;
 import java.util.Random;
 
 public class HomeTreeBranchFeature<T extends HomeTreeBranchConfig> extends Feature<T> {
-    private static final byte[] OTHER_COORD_PAIRS = {
-        2, 0, 0, 1, 2, 1
-    };
-    
+    private static final Direction.Axis[] ALL_AXIS = Direction.Axis.values();
+
     private final ConfiguredFeature<RainforestVinesConfig, RainforestVinesFeature> vinesFeature;
 
     public HomeTreeBranchFeature(Codec<T> codec) {
@@ -50,13 +49,13 @@ public class HomeTreeBranchFeature<T extends HomeTreeBranchConfig> extends Featu
         final int leafCircleSizeConstant = 3;
         final int y2 = pos.getY() + branchY2;
 
-        placeBlockLine(world, new int[] { branchX1, pos.getY(), branchZ1 }, new int[] {branchX2, y2, branchZ2 }, wood);
-        placeBlockLine(world, new int[] { branchX1 + 1, pos.getY(), branchZ1 }, new int[] {branchX2+ 1, y2, branchZ2 }, wood);
-        placeBlockLine(world, new int[] { branchX1 - 1, pos.getY(), branchZ1 }, new int[] {branchX2 - 1, y2, branchZ2 }, wood);
-        placeBlockLine(world, new int[] { branchX1, pos.getY(), branchZ1 + 1 }, new int[] {branchX2, y2, branchZ2 + 1 }, wood);
-        placeBlockLine(world, new int[] { branchX1, pos.getY(), branchZ1 - 1 }, new int[] {branchX2, y2, branchZ2 - 1 }, wood);
-        placeBlockLine(world, new int[] { branchX1, pos.getY() - 1, branchZ1 }, new int[] {branchX2, y2 - 1, branchZ2 }, wood);
-        placeBlockLine(world, new int[] { branchX1, pos.getY() + 1, branchZ1 }, new int[] {branchX2, y2 + 1, branchZ2 }, wood);
+        placeBlockLine(world, new BlockPos(branchX1, pos.getY(), branchZ1), new BlockPos(branchX2, y2, branchZ2 ), wood);
+        placeBlockLine(world, new BlockPos(branchX1 + 1, pos.getY(), branchZ1), new BlockPos(branchX2+ 1, y2, branchZ2), wood);
+        placeBlockLine(world, new BlockPos(branchX1 - 1, pos.getY(), branchZ1), new BlockPos(branchX2 - 1, y2, branchZ2), wood);
+        placeBlockLine(world, new BlockPos(branchX1, pos.getY(), branchZ1 + 1), new BlockPos(branchX2, y2, branchZ2 + 1), wood);
+        placeBlockLine(world, new BlockPos(branchX1, pos.getY(), branchZ1 - 1), new BlockPos(branchX2, y2, branchZ2 - 1), wood);
+        placeBlockLine(world, new BlockPos(branchX1, pos.getY() - 1, branchZ1), new BlockPos(branchX2, y2 - 1, branchZ2), wood);
+        placeBlockLine(world, new BlockPos(branchX1, pos.getY() + 1, branchZ1), new BlockPos(branchX2, y2 + 1, branchZ2), wood);
         genLeafCircle(world, branchX2, y2 - 1, branchZ2, leafCircleSizeConstant + 5, leafCircleSizeConstant + 3, leaf, true);
         genLeafCircle(world, branchX2, y2, branchZ2, leafCircleSizeConstant + 6, 0, leaf, true);
         genLeafCircle(world, branchX2, y2 + 1, branchZ2, leafCircleSizeConstant + 10, 0, leaf, true);
@@ -85,41 +84,43 @@ public class HomeTreeBranchFeature<T extends HomeTreeBranchConfig> extends Featu
         }
     }
 
-    private void placeBlockLine(final IWorld world, int[] ai, int[] ai1, BlockState state) {
-        int[] ai2 = {
-            0, 0, 0
-        };
-        byte byte0 = 0;
-        int j = 0;
-        for (; byte0 < 3; byte0++) {
-            ai2[byte0] = ai1[byte0] - ai[byte0];
-            if(Math.abs(ai2[byte0]) > Math.abs(ai2[j])) {
-                j = byte0;
-            }
-        }
+    private void placeBlockLine(final IWorld world, BlockPos from, BlockPos to, BlockState state) {
+        BlockPos delta = to.subtract(from);
+        Direction.Axis primaryAxis = getLongestAxis(delta);
 
-        if (ai2[j] == 0) {
+        int maxLength = Math.abs(getCoordinateAlong(delta, primaryAxis));
+        if (maxLength == 0) {
             return;
         }
-        byte byte1 = OTHER_COORD_PAIRS[j];
-        byte byte2 = OTHER_COORD_PAIRS[j + 3];
-        byte byte3;
-        if (ai2[j] > 0) {
-            byte3 = 1;
-        } else {
-            byte3 = -1;
+
+        double stepX = (double) getCoordinateAlong(delta, Direction.Axis.X) / maxLength;
+        double stepY = (double) getCoordinateAlong(delta, Direction.Axis.Y) / maxLength;
+        double stepZ = (double) getCoordinateAlong(delta, Direction.Axis.Z) / maxLength;
+
+        for (int length = 0; length <= maxLength; length++) {
+            BlockPos pos = new BlockPos(
+                    from.getX() + length * stepX + 0.5,
+                    from.getY() + length * stepY + 0.5,
+                    from.getZ() + length * stepZ + 0.5
+            );
+            world.setBlockState(pos, state, Constants.BlockFlags.DEFAULT);
         }
-        double d = (double) ai2[byte1] / (double) ai2[j];
-        double d1 = (double) ai2[byte2] / (double) ai2[j];
-        int[] ai3 = {
-                0, 0, 0
-        };
-        int k = 0;
-        for (int l = ai2[j] + byte3; k != l; k += byte3) {
-            ai3[j] = MathHelper.floor(ai[j] + k + 0.5D);
-            ai3[byte1] = MathHelper.floor(ai[byte1] + k * d + 0.5D);
-            ai3[byte2] = MathHelper.floor(ai[byte2] + k * d1 + 0.5D);
-            world.setBlockState(new BlockPos(ai3[0], ai3[1], ai3[2]), state, 3);
+    }
+
+    private Direction.Axis getLongestAxis(BlockPos delta) {
+        Direction.Axis longestAxis = Direction.Axis.X;
+        int longestLength = 0;
+        for (Direction.Axis axis : ALL_AXIS) {
+            int length = Math.abs(getCoordinateAlong(delta, axis));
+            if (length > longestLength) {
+                longestAxis = axis;
+                longestLength = length;
+            }
         }
+        return longestAxis;
+    }
+
+    private static int getCoordinateAlong(Vector3i pos, Direction.Axis axis) {
+        return axis.getCoordinate(pos.getX(), pos.getY(), pos.getZ());
     }
 }
