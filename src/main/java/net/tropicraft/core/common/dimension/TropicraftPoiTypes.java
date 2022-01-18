c00390223cf2079a2de2865d3f6a84e9aaf0769e
Dragon-Seeker <50533513+Dragon-Seeker@users.noreply.github.com>
2022-01-18 08:27:30 -0600

    Add a water block variant for the portal
    Also change Poi type register to use generics that will allow for any block that extends the base block class

--- src/main/java/net/tropicraft/core/common/dimension/TropicraftPoiTypes.java
+++ src/main/java/net/tropicraft/core/common/dimension/TropicraftPoiTypes.java
@@ -2,6 +2,7 @@ package net.tropicraft.core.common.dimension;
 
 import com.google.common.collect.ImmutableSet;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -21,11 +22,11 @@ public class TropicraftPoiTypes extends PoiType {
         super(p_27362_, p_27363_, p_27364_, p_27366_);
     }
 
-    private static RegistryObject<PoiType> registerPoi(String pKey, RegistryObject<PortalWaterBlock> block, int pMaxFreeTickets, int pValidRange) {
+    private static RegistryObject<PoiType> registerPoi(String pKey, RegistryObject<? extends Block> block, int pMaxFreeTickets, int pValidRange) {
         return POI_TYPE.register(pKey, () -> new TropicraftPoiTypes(pKey, getBlockStates(block), pMaxFreeTickets, pValidRange));
     }
 
-    private static Set<BlockState> getBlockStates(RegistryObject<PortalWaterBlock> pBlock) {
+    private static Set<BlockState> getBlockStates(RegistryObject<? extends Block> pBlock) {
         return ImmutableSet.copyOf(pBlock.get().getStateDefinition().getPossibleStates());
     }
 }
