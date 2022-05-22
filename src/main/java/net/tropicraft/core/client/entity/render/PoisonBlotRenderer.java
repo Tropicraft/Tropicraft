1ed828ddf8c172fd352ed32e9b1d5062f95d14b4
Gegy <gegy.dev@gmail.com>
2022-05-22 14:52:28 +0200

    Fix #439: crashing poison blot renderer

--- src/main/java/net/tropicraft/core/client/entity/render/PoisonBlotRenderer.java
+++ src/main/java/net/tropicraft/core/client/entity/render/PoisonBlotRenderer.java
@@ -2,11 +2,11 @@ package net.tropicraft.core.client.entity.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.renderer.MultiBufferSource;
 import com.mojang.math.Vector3f;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -27,12 +27,22 @@ public class PoisonBlotRenderer extends EntityRenderer<PoisonBlotEntity> {
         stack.mulPose(this.entityRenderDispatcher.cameraOrientation());
         stack.mulPose(Vector3f.YP.rotationDegrees(180.0F));
         final VertexConsumer buffer = TropicraftRenderUtils.getEntityCutoutBuilder(bufferIn, getTextureLocation(entity));
-        buffer.vertex(-.5, -.5, 0).uv(0, 1).endVertex();
-        buffer.vertex( .5, -.5, 0).uv(1, 1).endVertex();
-        buffer.vertex( .5,  .5, 0).uv(1, 0).endVertex();
-        buffer.vertex(-.5,  .5, 0).uv(0, 0).endVertex();
+        final PoseStack.Pose pose = stack.last();
+        vertex(pose, buffer, -.5f, -.5f, 0, 1, packedLightIn);
+        vertex(pose, buffer, .5f, -.5f, 1, 1, packedLightIn);
+        vertex(pose, buffer, .5f, .5f, 1, 0, packedLightIn);
+        vertex(pose, buffer, -.5f, .5f, 0, 0, packedLightIn);
         stack.popPose();
         super.render(entity, entityYaw, partialTicks, stack, bufferIn, packedLightIn);
+    }
+
+    private static void vertex(PoseStack.Pose pose, VertexConsumer buffer, float x, float y, float u, float v, int light) {
+        buffer.vertex(pose.pose(), x, y, 0f)
+                .color(255, 255, 255, 255)
+                .uv(u, v)
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(light)
+                .normal(pose.normal(), 0f, 0f, 1f).endVertex();
     }
 
     @Nullable
