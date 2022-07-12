22e22b05da3ea62acf9ccf037045436b0e72f91c
baguchan <shurf012@gmail.com>
2022-07-12 22:00:31 +0900

    oops

--- src/main/java/net/tropicraft/core/common/item/SpearItem.java
+++ src/main/java/net/tropicraft/core/common/item/SpearItem.java
@@ -2,11 +2,14 @@ package net.tropicraft.core.common.item;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -17,6 +20,7 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TridentItem;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.tropicraft.core.common.entity.projectile.SpearEntity;
 
 public class SpearItem extends TridentItem {
@@ -43,26 +47,58 @@ public class SpearItem extends TridentItem {
 			int i = this.getUseDuration(stack) - timeLeft;
 			if (i >= 10) {
 				int j = EnchantmentHelper.getRiptide(stack);
-				if (!worldIn.isClientSide) {
-					stack.hurtAndBreak(1, player, (p_43388_) -> {
-						p_43388_.broadcastBreakEvent(entityLiving.getUsedItemHand());
-					});
-					if (j == 0) {
-						SpearEntity thrownspear = new SpearEntity(worldIn, player, stack);
-						thrownspear.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 2.5F + (float) j * 0.5F, 1.0F);
-						if (player.getAbilities().instabuild) {
-							thrownspear.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
-						}
+				if (j <= 0 || player.isInWaterOrRain()) {
+					if (!worldIn.isClientSide) {
+						stack.hurtAndBreak(1, player, (p_43388_) -> {
+							p_43388_.broadcastBreakEvent(entityLiving.getUsedItemHand());
+						});
+						if (j == 0) {
+							SpearEntity thrownspear = new SpearEntity(worldIn, player, stack);
+							thrownspear.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 2.5F + (float) j * 0.5F, 1.0F);
+							if (player.getAbilities().instabuild) {
+								thrownspear.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
+							}
 
-						worldIn.addFreshEntity(thrownspear);
-						worldIn.playSound((Player) null, thrownspear, SoundEvents.TRIDENT_THROW, SoundSource.PLAYERS, 1.0F, 1.0F);
-						if (!player.getAbilities().instabuild) {
-							player.getInventory().removeItem(stack);
+							worldIn.addFreshEntity(thrownspear);
+							worldIn.playSound((Player) null, thrownspear, SoundEvents.TRIDENT_THROW, SoundSource.PLAYERS, 1.0F, 1.0F);
+							if (!player.getAbilities().instabuild) {
+								player.getInventory().removeItem(stack);
+							}
 						}
 					}
-				}
 
-				player.awardStat(Stats.ITEM_USED.get(this));
+					player.awardStat(Stats.ITEM_USED.get(this));
+					if (j > 0) {
+						float f7 = player.getYRot();
+						float f = player.getXRot();
+						float f1 = -Mth.sin(f7 * ((float) Math.PI / 180F)) * Mth.cos(f * ((float) Math.PI / 180F));
+						float f2 = -Mth.sin(f * ((float) Math.PI / 180F));
+						float f3 = Mth.cos(f7 * ((float) Math.PI / 180F)) * Mth.cos(f * ((float) Math.PI / 180F));
+						float f4 = Mth.sqrt(f1 * f1 + f2 * f2 + f3 * f3);
+						float f5 = 3.0F * ((1.0F + (float) j) / 4.0F);
+						f1 *= f5 / f4;
+						f2 *= f5 / f4;
+						f3 *= f5 / f4;
+						player.push((double) f1, (double) f2, (double) f3);
+						player.startAutoSpinAttack(20);
+						if (player.isOnGround()) {
+							float f6 = 1.1999999F;
+							player.move(MoverType.SELF, new Vec3(0.0D, (double) 1.1999999F, 0.0D));
+						}
+
+						SoundEvent soundevent;
+						if (j >= 3) {
+							soundevent = SoundEvents.TRIDENT_RIPTIDE_3;
+						} else if (j == 2) {
+							soundevent = SoundEvents.TRIDENT_RIPTIDE_2;
+						} else {
+							soundevent = SoundEvents.TRIDENT_RIPTIDE_1;
+						}
+
+						worldIn.playSound((Player) null, player, soundevent, SoundSource.PLAYERS, 1.0F, 1.0F);
+					}
+
+				}
 			}
 		}
 	}
