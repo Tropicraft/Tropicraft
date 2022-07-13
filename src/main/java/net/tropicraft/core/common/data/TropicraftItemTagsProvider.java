6765d2002c2eab883d7c3bb752271e2cd56a18b2
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2022-07-13 16:37:32 +0200

    Update TropicraftItemTagsProvider.java

--- src/main/java/net/tropicraft/core/common/data/TropicraftItemTagsProvider.java
+++ src/main/java/net/tropicraft/core/common/data/TropicraftItemTagsProvider.java
@@ -87,7 +87,7 @@ public class TropicraftItemTagsProvider extends ItemTagsProvider {
         appendToTag(ItemTags.MUSIC_DISCS, MUSIC_DISCS);
 
         // Copy block tags
-        copyBlockAndAppendToTag(TropicraftTags.Blocks.SAND, SAND, ItemTags.SAND); // IDK know about that
+        copy(TropicraftTags.Blocks.SAND, SAND);
         copy(TropicraftTags.Blocks.MUD, MUD);
 
         copyBlockAndAppendToTag(TropicraftTags.Blocks.SAPLINGS, SAPLINGS, ItemTags.SAPLINGS);
