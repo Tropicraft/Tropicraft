420a99e4d0ae5cbe3f683f200aedbf0b5c56aa7c
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2022-07-24 16:14:52 -0400

    Teach hammerheads how to swim
    - Old AI is left commented in case it is needed in the future
    Fixes #488

--- src/main/java/net/tropicraft/core/common/entity/underdasea/SharkEntity.java
+++ src/main/java/net/tropicraft/core/common/entity/underdasea/SharkEntity.java
@@ -8,25 +8,34 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.BossEvent;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.control.SmoothSwimmingLookControl;
+import net.minecraft.world.entity.ai.control.SmoothSwimmingMoveControl;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
+import net.minecraft.world.entity.ai.navigation.WaterBoundPathNavigation;
 import net.minecraft.world.entity.animal.WaterAnimal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.phys.HitResult;
 import net.tropicraft.core.common.entity.ai.fishies.AvoidWallsGoal;
 import net.tropicraft.core.common.entity.ai.fishies.RandomSwimGoal;
 import net.tropicraft.core.common.entity.ai.fishies.SwimToAvoidEntityGoal;
 import net.tropicraft.core.common.entity.ai.fishies.TargetPreyGoal;
 import net.tropicraft.core.common.item.TropicraftItems;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
 
-public class SharkEntity extends TropicraftFishEntity {
+public class SharkEntity extends WaterAnimal {
 
     private static final EntityDataAccessor<Boolean> IS_BOSS = SynchedEntityData.defineId(SharkEntity.class, EntityDataSerializers.BOOLEAN);
 
@@ -37,7 +46,9 @@ public class SharkEntity extends TropicraftFishEntity {
     public SharkEntity(EntityType<? extends WaterAnimal> type, Level world) {
         super(type, world);
         xpReward = 20;
-        this.setApproachesPlayers(true);
+        this.moveControl = new SmoothSwimmingMoveControl(this, 85, 10, 0.02F, 0.1F, true);
+        this.lookControl = new SmoothSwimmingLookControl(this, 10);
+//        this.setApproachesPlayers(true);
         // TODO - this.setDropStack(ItemRegistry.fertilizer, 3);
     }
 
@@ -45,12 +56,25 @@ public class SharkEntity extends TropicraftFishEntity {
     protected void registerGoals() {
         super.registerGoals();
         //goalSelector.addGoal(0, new EntityAISwimAvoidPredator(0, this, 2D));
-        goalSelector.addGoal(0, new AvoidWallsGoal(EnumSet.of(Goal.Flag.MOVE), this));
-        if (fleeFromPlayers) {
-            goalSelector.addGoal(0, new SwimToAvoidEntityGoal(EnumSet.of(Goal.Flag.MOVE), this, 5F, new Class[] {Player.class}));
-        }
-        goalSelector.addGoal(2, new TargetPreyGoal(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK), this));
-        goalSelector.addGoal(2, new RandomSwimGoal(EnumSet.of(Goal.Flag.MOVE), this));
+//        goalSelector.addGoal(0, new AvoidWallsGoal(EnumSet.of(Goal.Flag.MOVE), this));
+        this.goalSelector.addGoal(0, new BreathAirGoal(this));
+        this.goalSelector.addGoal(0, new TryFindWaterGoal(this));
+        this.goalSelector.addGoal(4, new RandomSwimmingGoal(this, 1.0D, 10));
+        this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 6.0F));
+
+//        if (fleeFromPlayers) {
+//            goalSelector.addGoal(0, new SwimToAvoidEntityGoal(EnumSet.of(Goal.Flag.MOVE), this, 5F, new Class[] {Player.class}));
+//        }
+//        goalSelector.addGoal(2, new TargetPreyGoal(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK), this));
+//        goalSelector.addGoal(2, new RandomSwimGoal(EnumSet.of(Goal.Flag.MOVE), this));
+    }
+
+    @Nullable
+    @Override
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor pLevel, DifficultyInstance pDifficulty, MobSpawnType pReason, @Nullable SpawnGroupData pSpawnData, @Nullable CompoundTag pDataTag) {
+        this.setAirSupply(this.getMaxAirSupply());
+        return super.finalizeSpawn(pLevel, pDifficulty, pReason, pSpawnData, pDataTag);
     }
 
     @Override
@@ -62,6 +86,7 @@ public class SharkEntity extends TropicraftFishEntity {
     public static AttributeSupplier.Builder createAttributes() {
         return WaterAnimal.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 10.0)
+                .add(Attributes.MOVEMENT_SPEED, 1.2)
                 .add(Attributes.ATTACK_DAMAGE, 4.0);
     }
 
@@ -78,7 +103,7 @@ public class SharkEntity extends TropicraftFishEntity {
         //TODO this.setDropStack(ItemRegistry.yellowFlippers, 1);
         setCustomName(new TextComponent("Elder Hammerhead"));
         setCustomNameVisible(true);
-        setSwimSpeeds(1.1f, 2.2f, 1.5f, 3f, 5f);
+//        setSwimSpeeds(1.1f, 2.2f, 1.5f, 3f, 5f);
         getAttribute(Attributes.MAX_HEALTH).setBaseValue(20);
         // TODO in renderer - this.setTexture("hammerhead4");
         if (!level.isClientSide) {
@@ -90,42 +115,47 @@ public class SharkEntity extends TropicraftFishEntity {
     @Override
     public void tick() {
         super.tick();
-        if (isBoss()) {
-            // Set state to boss
-            if (!hasSetBoss) {
-                setBossTraits();
-            }
+//        if (isBoss()) {
+//            // Set state to boss
+//            if (!hasSetBoss) {
+//                setBossTraits();
+//            }
+//
+//            if (!level.isClientSide) {
+//                // Search for suitable target
+//                Player nearest = level.getNearestPlayer(this, 64D);
+//                if (nearest != null) {
+//                    if (hasLineOfSight(nearest) && nearest.isInWater() && !nearest.isCreative() && nearest.isAlive()) {
+//                        aggressTarget = nearest;
+//                        setTargetHeading(aggressTarget.getX(), aggressTarget.getY() + 1, aggressTarget.getZ(), true);
+//                        // Show health bar to target player
+//                        if (nearest instanceof ServerPlayer) {
+//                            if (!bossInfo.getPlayers().contains(nearest)) {
+//                                bossTargets.add((ServerPlayer) nearest);
+//                                bossInfo.addPlayer((ServerPlayer) nearest);
+//                            }
+//                        }
+//                    } else {
+//                        clearBossTargets();
+//                    }
+//                } else {
+//                    clearBossTargets();
+//                }
+//
+//                // Heal if no target
+//                if (this.getHealth() < this.getMaxHealth() && this.tickCount % 80 == 0 && this.aggressTarget == null) {
+//                    this.heal(1f);
+//                    this.spawnAnim();
+//                }
+//                // Update health bar
+//                this.bossInfo.setProgress(this.rangeMap(this.getHealth(), 0, this.getMaxHealth(), 0, 1));
+//            }
+//        }
+    }
 
-            if (!level.isClientSide) {
-                // Search for suitable target
-                Player nearest = level.getNearestPlayer(this, 64D);
-                if (nearest != null) {
-                    if (hasLineOfSight(nearest) && nearest.isInWater() && !nearest.isCreative() && nearest.isAlive()) {
-                        aggressTarget = nearest;
-                        setTargetHeading(aggressTarget.getX(), aggressTarget.getY() + 1, aggressTarget.getZ(), true);
-                        // Show health bar to target player
-                        if (nearest instanceof ServerPlayer) {
-                            if (!bossInfo.getPlayers().contains(nearest)) {
-                                bossTargets.add((ServerPlayer) nearest);
-                                bossInfo.addPlayer((ServerPlayer) nearest);
-                            }
-                        }
-                    } else {
-                        clearBossTargets();
-                    }
-                } else {
-                    clearBossTargets();
-                }
-
-                // Heal if no target
-                if (this.getHealth() < this.getMaxHealth() && this.tickCount % 80 == 0 && this.aggressTarget == null) {
-                    this.heal(1f);
-                    this.spawnAnim();
-                }
-                // Update health bar
-                this.bossInfo.setProgress(this.rangeMap(this.getHealth(), 0, this.getMaxHealth(), 0, 1));
-            }
-        }
+    @Override
+    protected PathNavigation createNavigation(Level pLevel) {
+        return new WaterBoundPathNavigation(this, pLevel);
     }
 
     private void clearBossTargets() {
