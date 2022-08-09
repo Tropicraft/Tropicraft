9b863f02e05b447c38ed0f6d6580749d4ae52d60
Dragon-Seeker <timekeeperguild@gmail.com>
2022-08-09 15:58:01 -0500

    Add ability to Bonemeal mud for reeds

--- src/main/java/net/tropicraft/core/common/block/MudBlock.java
+++ src/main/java/net/tropicraft/core/common/block/MudBlock.java
@@ -2,9 +2,12 @@ package net.tropicraft.core.common.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -13,8 +16,11 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.IPlantable;
+import net.tropicraft.core.common.TropicraftTags;
 
-public final class MudBlock extends Block {
+import java.util.Random;
+
+public final class MudBlock extends Block implements BonemealableBlock {
     private static final VoxelShape SHAPE = Block.box(0.0, 0.0, 0.0, 16.0, 14.0, 16.0);
 
     public MudBlock(Properties properties) {
@@ -51,5 +57,37 @@ public final class MudBlock extends Block {
     @OnlyIn(Dist.CLIENT)
     public float getShadeBrightness(BlockState state, BlockGetter world, BlockPos pos) {
         return 0.2F;
+    }
+
+    public boolean isValidBonemealTarget(BlockGetter pLevel, BlockPos pPos, BlockState pState, boolean pIsClient) {
+        return pLevel.getBlockState(pPos.above()).isAir();
+    }
+
+    public boolean isBonemealSuccess(Level pLevel, Random pRand, BlockPos pPos, BlockState pState) {
+        return true;
+    }
+
+    public void performBonemeal(ServerLevel pLevel, Random pRand, BlockPos pPos, BlockState pState) {
+        mainLoop:
+        for(int i = 0; i < 96; ++i) {
+            BlockPos blockpos1 = pPos.above();
+
+            for(int j = 0; j < i / 16; ++j) {
+                blockpos1 = blockpos1.offset(pRand.nextInt(3) - 1, (pRand.nextInt(3) - 1) * pRand.nextInt(3) / 2, pRand.nextInt(3) - 1);
+                if (!pLevel.getBlockState(blockpos1.below()).is(TropicraftTags.Blocks.MUD) || pLevel.getBlockState(blockpos1).isCollisionShapeFullBlock(pLevel, blockpos1)) {
+                    continue mainLoop;
+                }
+            }
+
+            BlockState blockstate1 = pLevel.getBlockState(blockpos1);
+            if (blockstate1.is(TropicraftTags.Blocks.MUD) && pRand.nextInt(10) == 0) {
+                ((BonemealableBlock)TropicraftBlocks.MUD.get()).performBonemeal(pLevel, pRand, blockpos1, blockstate1);
+            } else if (blockstate1.isAir()) {
+                if (pRand.nextInt(2) == 0) {
+                    pLevel.setBlock(blockpos1, TropicraftBlocks.REEDS.get().defaultBlockState(), Block.UPDATE_ALL);
+                }
+            }
+        }
+
     }
 }
