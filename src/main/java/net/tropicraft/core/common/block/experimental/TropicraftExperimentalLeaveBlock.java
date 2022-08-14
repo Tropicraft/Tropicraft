4e12274f95fbfe7b1b2017333edb78ee0116fe8d
Dragon-Seeker <timekeeperguild@gmail.com>
2022-08-14 17:42:58 -0500

    Remove need for Diagonal Directions Enum and overall clean up and reformat class

--- src/main/java/net/tropicraft/core/common/block/experimental/TropicraftExperimentalLeaveBlock.java
+++ src/main/java/net/tropicraft/core/common/block/experimental/TropicraftExperimentalLeaveBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -15,27 +16,32 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 import java.util.Random;
 
+@SuppressWarnings("NullableProblems")
 public class TropicraftExperimentalLeaveBlock extends LeavesBlock {
 
-    public static IntegerProperty EXTRADISTANCE;
-    public static BooleanProperty TEMPIGNORERANDOMTICK; //Property to check if there has even been a block update to prevent unwanted decay instantly
+    private static final int MAX_DISTANCE = 20;
+
+    public static IntegerProperty CUSTOM_DISTANCE = IntegerProperty.create("custom_distance", 1, MAX_DISTANCE);
+
+    //Property to check if there has even been a block update to prevent unwanted decay instantly
+    public static BooleanProperty TEMPORARY_IGNORE_DECAY = BooleanProperty.create("temp_ignore_decay");
 
     public TropicraftExperimentalLeaveBlock(Properties settings) {
         super(settings);
-        this.registerDefaultState(this.defaultBlockState().setValue(DISTANCE, 7).setValue(EXTRADISTANCE, 20).setValue(TEMPIGNORERANDOMTICK, false));
+
+        this.registerDefaultState(this.defaultBlockState().setValue(CUSTOM_DISTANCE, MAX_DISTANCE).setValue(TEMPORARY_IGNORE_DECAY, false));
     }
 
     @Override
     public boolean isRandomlyTicking(BlockState state) { //*
-        //Checks if it is set Persistent but also checks for the custom leaf distance
-        return state.getValue(EXTRADISTANCE) == 20 && !(Boolean)state.getValue(PERSISTENT);
+        return state.getValue(CUSTOM_DISTANCE) == MAX_DISTANCE && !(Boolean)state.getValue(PERSISTENT);
     }
 
     //Similar to LeavesBlock Random tick but just checks for the changed Distance
     @Override
     public void randomTick(BlockState state, ServerLevel world, BlockPos pos, Random random) {
         //Checks for leaf's extra distance and also checks the first cycle value to prevent random decay to happen prematurely
-        if (!state.getValue(PERSISTENT) && state.getValue(EXTRADISTANCE) == 20 && state.getValue(TEMPIGNORERANDOMTICK)) {
+        if (!state.getValue(PERSISTENT) && state.getValue(CUSTOM_DISTANCE) == MAX_DISTANCE && state.getValue(TEMPORARY_IGNORE_DECAY)) {
             dropResources(state, world, pos);
             world.removeBlock(pos, false);
         }
@@ -46,72 +52,77 @@ public class TropicraftExperimentalLeaveBlock extends LeavesBlock {
         world.setBlock(pos, updateDistanceFromLeaves(state, world, pos), 3);
     }
 
-    public BlockState updateShapeBlackList(BlockState state, Direction direction, BlockState neighborState, LevelAccessor world, BlockPos pos, BlockPos neighborPos, boolean blackListActive) {
-        int i = getDistanceFromLeave(neighborState, blackListActive) + 1;
-        if (i != 1 || state.getValue(EXTRADISTANCE) >= i) {
-            world.scheduleTick(pos, this, 1);
+    @Override
+    public BlockState updateShape(BlockState state, Direction facing, BlockState neighborState, LevelAccessor world, BlockPos currentPos, BlockPos neighborPos) {
+        int i = getDistanceFromLeave(neighborState, isBlackListedLogPos(currentPos, neighborPos)) + 1;
+        if (i != 1 || state.getValue(CUSTOM_DISTANCE) >= i) {
+            world.scheduleTick(currentPos, this, 1);
         }
 
         return state;
     }
 
-    // Similar to base leaf block but with the diagonal directions
-    private static BlockState updateDistanceFromLeaves(BlockState state, LevelAccessor world, BlockPos pos) { //*
-        BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
-        DiagonalDirections[] directionDiagonals = DiagonalDirections.values();
+    //Used to update diagonal or Indirect blocks to propagate the extra distance value for our leaves
+    @Override
+    public void updateIndirectNeighbourShapes(BlockState state, LevelAccessor world, BlockPos pos, int flags, int maxUpdateDepth) {
+        for(BlockPos neighbourPos : BlockPos.betweenClosed(pos.offset(-1,-1,-1), pos.offset(1,1,1))){
+            if(neighbourPos.equals(pos)) continue;
 
-        int currentExtraDistance = 20; //*
-        int length = directionDiagonals.length;
+            BlockState neighbourState = world.getBlockState(neighbourPos);
+            if (neighbourState != Blocks.AIR.defaultBlockState() && neighbourState.getBlock() == this) {
+                BlockState updatedNeighborState = updateShape(neighbourState, Direction.UP, state, world, neighbourPos, pos); //isBlackListedLogPos(dx, dy, dz)
+                Block.updateOrDestroy(neighbourState, updatedNeighborState, world, neighbourPos, flags, maxUpdateDepth);
+            }
+        }
+    }
 
-        for(int i = 0; i < length; ++i) {
-            DiagonalDirections direction = directionDiagonals[i];
-            mutable.setWithOffset(pos, direction.getX(), direction.getY(), direction.getZ());
-            currentExtraDistance = Math.min(currentExtraDistance, getDistanceFromLeave(world.getBlockState(mutable), isBlackListedLogPos(direction)) + direction.additionAmount);
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) { //*
+        builder.add(PERSISTENT, CUSTOM_DISTANCE, TEMPORARY_IGNORE_DECAY);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return updateDistanceFromLeaves(this.defaultBlockState().setValue(PERSISTENT, Boolean.valueOf(true)), pContext.getLevel(), pContext.getClickedPos());
+    }
+
+    //------------------------------------------------------------------------------------------------
+
+    private static BlockState updateDistanceFromLeaves(BlockState state, LevelAccessor world, BlockPos pos) {
+        int currentExtraDistance = MAX_DISTANCE;
+
+        for(BlockPos neighborPos : BlockPos.betweenClosed(pos.offset(-1,-1,-1), pos.offset(1,1,1))){
+            if(neighborPos.equals(pos)) continue;
+
+            int distanceFromOrigin = neighborPos.distManhattan(pos);
+
+            currentExtraDistance = Math.min(currentExtraDistance, getDistanceFromLeave(world.getBlockState(neighborPos), isBlackListedLogPos(neighborPos, pos)) + distanceFromOrigin); //isBlackListedLogPos(dx, dy, dz)
             if (currentExtraDistance == 1) {
                 break;
             }
         }
 
-        return state.setValue(EXTRADISTANCE, currentExtraDistance).setValue(TEMPIGNORERANDOMTICK, true);
-    }
-
-    //Used to update diagonal or Indirect blocks to propagate the extra distance value for our leaves
-    @Override
-    public void updateIndirectNeighbourShapes(BlockState state, LevelAccessor world, BlockPos pos, int flags, int maxUpdateDepth) {
-        BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
-        DiagonalDirections[] directions = DiagonalDirections.values();
-        int LENGTH = directions.length;
-
-        for (int index = 0; index < LENGTH; ++index) {
-            DiagonalDirections direction = directions[index];
-            mutable.setWithOffset(pos, direction.getX(), direction.getY(), direction.getZ());
-            BlockState blockState = world.getBlockState(mutable);
-            if (blockState != Blocks.AIR.defaultBlockState() && blockState.getBlock() == this) {
-                BlockState blockState2 = updateShapeBlackList(blockState, Direction.UP, state, world, mutable, pos, isBlackListedLogPos(direction));
-                Block.updateOrDestroy(blockState, blockState2, world, mutable, flags, maxUpdateDepth);
-            }
-        }
+        return state.setValue(CUSTOM_DISTANCE, currentExtraDistance).setValue(TEMPORARY_IGNORE_DECAY, true);
     }
 
     //Used as to get distance info from log block if not blacklisted and distance info from all leaf block's around it
     private static int getDistanceFromLeave(BlockState state, boolean blackListBlock) {
-        return (state.is(BlockTags.LOGS) && !blackListBlock) ? 0 : ((state.getBlock() instanceof TropicraftExperimentalLeaveBlock) ? state.getValue(EXTRADISTANCE) : 20);
+        return (state.is(BlockTags.LOGS) && !blackListBlock) ? 0 : ((state.getBlock() instanceof TropicraftExperimentalLeaveBlock) ? state.getValue(CUSTOM_DISTANCE) : MAX_DISTANCE);
     }
 
-    //Used to black list logs in the bottom layer of checks surrounding the bottom block
-    private static boolean isBlackListedLogPos(DiagonalDirections direction){
-        return direction.getY() == -1 && !(direction.getX() == 0 && direction.getZ() == 0);
-    }
+    /**
+     * Used to black list logs in the bottom layer of checks surrounding the bottom block.
+     *
+     * There is an issue if you include a ring of logs under a central block, it will cause
+     * the leave block never to be properly updated due to the logs not diagonally block updating the leaves
+     */
 
-    //Still holds the default Property values of leaves just encase
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) { //*
-        builder.add(DISTANCE, PERSISTENT, EXTRADISTANCE, TEMPIGNORERANDOMTICK);
-    }
+    private static boolean isBlackListedLogPos(BlockPos currentPos, BlockPos neighborPos){
+        int dy = neighborPos.getY() - currentPos.getY();
+        int dx = neighborPos.getX() - currentPos.getX();
+        int dz = neighborPos.getZ() - currentPos.getZ();
 
-    static {
-        EXTRADISTANCE = IntegerProperty.create("extradistance", 1, 20);
-        TEMPIGNORERANDOMTICK = BooleanProperty.create("pastfirstcycle");
+        return dy == -1 && !(dx == 0 && dz == 0);
     }
 
 }
