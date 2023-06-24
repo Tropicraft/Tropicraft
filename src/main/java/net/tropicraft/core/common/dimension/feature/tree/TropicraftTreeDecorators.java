615f06cc88bea280a7514ff61c444b324ca0aca2
Gegy <gegy.dev@gmail.com>
2023-06-24 19:47:11 +0200

    Replace PianguasTreeDecorator with a configurable ReplaceInSoilDecorator

--- src/main/java/net/tropicraft/core/common/dimension/feature/tree/TropicraftTreeDecorators.java
+++ src/main/java/net/tropicraft/core/common/dimension/feature/tree/TropicraftTreeDecorators.java
@@ -7,13 +7,13 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.tropicraft.Constants;
-import net.tropicraft.core.common.dimension.feature.tree.mangrove.PianguasTreeDecorator;
+import net.tropicraft.core.common.dimension.feature.tree.mangrove.ReplaceInSoilDecorator;
 import net.tropicraft.core.common.dimension.feature.tree.mangrove.PneumatophoresTreeDecorator;
 
 public final class TropicraftTreeDecorators {
     public static final DeferredRegister<TreeDecoratorType<?>> REGISTER = DeferredRegister.create(ForgeRegistries.TREE_DECORATOR_TYPES, Constants.MODID);
 
-    public static RegistryObject<TreeDecoratorType<?>> PIANGUAS = register("pianguas", PianguasTreeDecorator.CODEC);
+    public static RegistryObject<TreeDecoratorType<?>> REPLACE_IN_SOIL = register("replace_in_soil", ReplaceInSoilDecorator.CODEC);
     public static RegistryObject<TreeDecoratorType<?>> PNEUMATOPHORES = register("pneumatophores", PneumatophoresTreeDecorator.CODEC);
     public static RegistryObject<TreeDecoratorType<?>> PAPAYA = register("papaya", PapayaTreeDecorator.CODEC);
 
--- src/main/java/net/tropicraft/core/common/dimension/feature/tree/mangrove/ReplaceInSoilDecorator.java
+++ src/main/java/net/tropicraft/core/common/dimension/feature/tree/mangrove/ReplaceInSoilDecorator.java
@@ -5,32 +5,37 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelSimulatedReader;
+import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
+import net.minecraft.world.level.levelgen.feature.stateproviders.RuleBasedBlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecorator;
 import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecoratorType;
 import net.tropicraft.core.common.Util;
 import net.tropicraft.core.common.block.TropicraftBlocks;
 import net.tropicraft.core.common.dimension.feature.tree.TropicraftTreeDecorators;
 
-public class PianguasTreeDecorator extends TreeDecorator {
-    public static final PianguasTreeDecorator REGULAR = new PianguasTreeDecorator(8, 4);
-    public static final PianguasTreeDecorator SMALL = new PianguasTreeDecorator(2, 2);
-
-    public static final Codec<PianguasTreeDecorator> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+public class ReplaceInSoilDecorator extends TreeDecorator {
+    public static final Codec<ReplaceInSoilDecorator> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.INT.fieldOf("count").forGetter(c -> c.count),
-            Codec.INT.fieldOf("spread").forGetter(c -> c.spread)
-    ).apply(instance, PianguasTreeDecorator::new));
+            Codec.INT.fieldOf("spread").forGetter(c -> c.spread),
+            RuleBasedBlockStateProvider.CODEC.fieldOf("state_provider").forGetter(c -> c.stateProvider),
+            BlockPredicate.CODEC.fieldOf("target").forGetter(c -> c.target)
+    ).apply(instance, ReplaceInSoilDecorator::new));
 
     private final int count;
     private final int spread;
+    private final RuleBasedBlockStateProvider stateProvider;
+    private final BlockPredicate target;
 
-    public PianguasTreeDecorator(int count, int spread) {
+    public ReplaceInSoilDecorator(int count, int spread, RuleBasedBlockStateProvider stateProvider, BlockPredicate target) {
         this.count = count;
         this.spread = spread;
+        this.stateProvider = stateProvider;
+        this.target = target;
     }
 
     @Override
     protected TreeDecoratorType<?> type() {
-        return TropicraftTreeDecorators.PIANGUAS.get();
+        return TropicraftTreeDecorators.REPLACE_IN_SOIL.get();
     }
 
     @Override
