5ecd91599dcd85b7d4712afffcd87612b72586e7
Cojomax99 <cojomax99@gmail.com>
2023-10-18 23:20:41 -0400

    Add Slender Harvest Mouse
    Closes #562.
    
    Co-authored-by: Gegy <gegy.dev@gmail.com>

---
+++ src/main/java/net/tropicraft/core/client/entity/render/SlenderHarvestMouseRenderer.java
@@ -0,0 +1,23 @@
+package net.tropicraft.core.client.entity.render;
+
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.tropicraft.Constants;
+import net.tropicraft.core.client.TropicraftRenderLayers;
+import net.tropicraft.core.client.entity.model.SlenderHarvestMouseModel;
+import net.tropicraft.core.common.entity.passive.SlenderHarvestMouseEntity;
+
+public class SlenderHarvestMouseRenderer extends MobRenderer<SlenderHarvestMouseEntity, SlenderHarvestMouseModel<SlenderHarvestMouseEntity>> {
+    private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(Constants.MODID, "textures/entity/slender_harvest_mouse.png");
+
+    public SlenderHarvestMouseRenderer(final EntityRendererProvider.Context context) {
+        super(context, new SlenderHarvestMouseModel<>(context.bakeLayer(TropicraftRenderLayers.SLENDER_HARVEST_MOUSE_LAYER)), 0.15F);
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(SlenderHarvestMouseEntity entity) {
+        return TEXTURE;
+    }
+
+}
