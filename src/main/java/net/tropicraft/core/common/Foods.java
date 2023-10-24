3b6ba173e1e6499a21bd4788a2eddb4779f69486
Gegy <gegy.dev@gmail.com>
2023-10-24 21:11:01 +0200

    Add basic Plaintain blocks, Passionfruit, Jocote item, and Mahogany Nut
    Co-authored-by: HexaBlu <Hexablu@gmail.com>

--- src/main/java/net/tropicraft/core/common/Foods.java
+++ src/main/java/net/tropicraft/core/common/Foods.java
@@ -7,8 +7,11 @@ public class Foods {
     public static final FoodProperties LIME = new FoodProperties.Builder().nutrition(2).saturationModifier(0.2f).fast().build();
     public static final FoodProperties GRAPEFRUIT = new FoodProperties.Builder().nutrition(2).saturationModifier(0.2f).fast().build();
     public static final FoodProperties ORANGE = new FoodProperties.Builder().nutrition(2).saturationModifier(0.2f).fast().build();
+    public static final FoodProperties PASSIONFRUIT = new FoodProperties.Builder().nutrition(2).saturationModifier(0.2f).fast().build();
+    public static final FoodProperties JOCOTE = new FoodProperties.Builder().nutrition(2).saturationModifier(0.2f).fast().build();
     public static final FoodProperties PAPAYA = new FoodProperties.Builder().nutrition(6).saturationModifier(0.5f).build();
     public static final FoodProperties PINEAPPLE_CUBES = new FoodProperties.Builder().nutrition(1).saturationModifier(0.1f).fast().build();
+    public static final FoodProperties PLANTAIN = new FoodProperties.Builder().nutrition(3).saturationModifier(0.2f).build();
     public static final FoodProperties COCONUT_CHUNK = new FoodProperties.Builder().nutrition(1).saturationModifier(0.1f).fast().build();
     public static final FoodProperties COOKED_RAY = new FoodProperties.Builder().nutrition(5).saturationModifier(0.5f).build();
     public static final FoodProperties FRESH_MARLIN = new FoodProperties.Builder().nutrition(2).saturationModifier(0.3f).build();
