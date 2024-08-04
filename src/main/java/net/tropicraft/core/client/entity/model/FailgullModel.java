1cc3940291de147e1bef7d1ba897fa7f9b1abbe7
Cojomax99 <cojomax99@gmail.com>
2024-08-04 16:19:03 +0200

    Fix failgull model, fix failgull wing rotation and positioning

--- src/main/java/net/tropicraft/core/client/entity/model/FailgullModel.java
+++ src/main/java/net/tropicraft/core/client/entity/model/FailgullModel.java
@@ -3,14 +3,15 @@ package net.tropicraft.core.client.entity.model;
 import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.CubeListBuilder;
-import net.minecraft.client.model.geom.builders.LayerDefinition;
-import net.minecraft.client.model.geom.builders.MeshDefinition;
-import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.util.Mth;
 import net.tropicraft.core.common.entity.passive.FailgullEntity;
 
 public class FailgullModel extends HierarchicalModel<FailgullEntity> {
+    private static final float WING_ROT_BASE = 90 * Mth.DEG_TO_RAD;
+    private static final float LEFT_WING_ZROT = 93 * Mth.DEG_TO_RAD;
+    private static final float RIGHT_WING_ZROT = 87 * Mth.DEG_TO_RAD;
+
     private final ModelPart root;
     private final ModelPart lowerLeg1;
     private final ModelPart lowerLeg2;
@@ -32,13 +33,13 @@ public class FailgullModel extends HierarchicalModel<FailgullEntity> {
         root.addOrReplaceChild("baseFootLeft",
                 CubeListBuilder.create()
                         .texOffs(0, 0)
-                        .addBox(0.0f, 0.0f, 0.0f, 1, 0, 1),
+                        .addBox(0.0f, 0.0f, 0.0f, 1, 0, 1, new CubeDeformation(0.001f)),
                 PartPose.offset(-1.0f, 23.0f, 0.0f));
 
         root.addOrReplaceChild("baseFootRight",
                 CubeListBuilder.create()
                         .texOffs(0, 0)
-                        .addBox(0.0f, 0.0f, 0.0f, 1, 0, 1),
+                        .addBox(0.0f, 0.0f, 0.0f, 1, 0, 1, new CubeDeformation(0.001f)),
                 PartPose.offset(1.0f, 23.0f, 0.0f));
 
         root.addOrReplaceChild("lowerLeg1",
@@ -74,14 +75,14 @@ public class FailgullModel extends HierarchicalModel<FailgullEntity> {
         root.addOrReplaceChild("rightWing",
                 CubeListBuilder.create()
                         .texOffs(0, 0)
-                        .addBox(0.0f, 0.0f, 0.0f, 0, 2, 5),
-                PartPose.offsetAndRotation(-1.0f, 18.0f, 0.0f, -0.06981f, -0.06981f, 0));
+                        .addBox(0.0f, 0.0f, 0.0f, 0, 2, 5, new CubeDeformation(0.001f)),
+                PartPose.offsetAndRotation(-1.0f, 18.0f, 0.0f, -WING_ROT_BASE, 0, RIGHT_WING_ZROT));
 
         root.addOrReplaceChild("leftWing",
                 CubeListBuilder.create()
                         .texOffs(0, 0)
-                        .addBox(0.0f, 0.0f, 0.0f, 0, 2, 5),
-                PartPose.offsetAndRotation(2.0f, 18.0f, 0.0f, -0.06981f, 0.06981f, 0));
+                        .addBox(0.0f, 0.0f, 0.0f, 0, 2, 5, new CubeDeformation(0.001f)),
+                PartPose.offsetAndRotation(2.0f, 18.0f, -2.0f, WING_ROT_BASE, 0, LEFT_WING_ZROT));
 
         root.addOrReplaceChild("neck",
                 CubeListBuilder.create()
@@ -102,8 +103,9 @@ public class FailgullModel extends HierarchicalModel<FailgullEntity> {
     public void setupAnim(FailgullEntity failgull, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
         lowerLeg1.xRot = Mth.cos(limbSwing * 0.6662f) * 1.4f * limbSwingAmount;
         lowerLeg2.xRot = Mth.cos(limbSwing * 0.6662f + Mth.PI) * 1.4f * limbSwingAmount;
-        rightWing.zRot = ageInTicks;
-        leftWing.zRot = -ageInTicks;
+
+        rightWing.zRot = RIGHT_WING_ZROT + Mth.sin(ageInTicks * 0.4f) / 2;
+        leftWing.zRot = LEFT_WING_ZROT - Mth.sin(ageInTicks * 0.4f) / 2;
     }
 
     @Override
