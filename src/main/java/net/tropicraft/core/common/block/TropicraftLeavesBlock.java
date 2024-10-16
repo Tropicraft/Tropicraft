48e2afd4b7becd886f18b89af67bcd31fddcdce4
bconlon <bconlon.gml@gmail.com>
2024-10-15 21:13:49 -0700

    allow leaf decay for palm-like leaves

--- src/main/java/net/tropicraft/core/common/block/TropicraftLeavesBlock.java
+++ src/main/java/net/tropicraft/core/common/block/TropicraftLeavesBlock.java
@@ -2,13 +2,27 @@ package net.tropicraft.core.common.block;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import org.joml.Vector3i;
+
+import java.util.List;
+import java.util.OptionalInt;
 
 public class TropicraftLeavesBlock extends LeavesBlock {
     public static final MapCodec<TropicraftLeavesBlock> CODEC = simpleCodec(TropicraftLeavesBlock::new);
+
+    public static final List<BlockPos> AROUND_OFFSETS = BlockPos.betweenClosedStream(-1, -1, -1, 1, 1, 1).map(BlockPos::immutable).filter((e) -> Vector3i.length(e.getX(), e.getY(), e.getZ()) != 0).toList();
+    public static final List<BlockPos> INDIRECT_NEIGHBOR_OFFSETS = BlockPos.betweenClosedStream(-1, -1, -1, 1, 1, 1).map(BlockPos::immutable).filter((e) -> Vector3i.length(e.getX(), e.getY(), e.getZ()) > 1).toList();
 
     public TropicraftLeavesBlock(Properties props) {
         super(props);
@@ -20,17 +34,75 @@ public class TropicraftLeavesBlock extends LeavesBlock {
     }
 
     @Override
-    public boolean isRandomlyTicking(BlockState state) {
-        return false;
+    protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (this.decaying(state)) {
+            dropResources(state, level, pos);
+            level.removeBlock(pos, false);
+        }
     }
 
     @Override
-    public void randomTick(BlockState state, ServerLevel worldIn, BlockPos pos, RandomSource random) {
-        // ignore decay
+    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        level.setBlock(pos, updateDistance(state, level, pos), 3);
     }
 
     @Override
-    public void tick(BlockState state, ServerLevel worldIn, BlockPos pos, RandomSource random) {
-        // ignore decay
+    protected void updateIndirectNeighbourShapes(BlockState state, LevelAccessor level, BlockPos pos, int flags, int recursionLeft) {
+        BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
+
+        for (Vec3i offset : INDIRECT_NEIGHBOR_OFFSETS) {
+            blockpos$mutableblockpos.setWithOffset(pos, offset);
+            if (level.getBlockState(blockpos$mutableblockpos).is(this)) {
+                level.neighborShapeChanged(Direction.getNearest(offset.getX(), offset.getY(), offset.getZ()).getOpposite(), state, blockpos$mutableblockpos, pos, flags, recursionLeft);
+            }
+        }
+    }
+
+    @Override
+    protected BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
+        if (state.getValue(WATERLOGGED)) {
+            level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+
+        int i = getDistanceAt(facingState) + 1;
+        if (i != 1 || state.getValue(DISTANCE) != i) {
+            level.scheduleTick(currentPos, this, 1);
+        }
+
+        return state;
+    }
+
+    private static BlockState updateDistance(BlockState state, LevelAccessor level, BlockPos pos) {
+        int i = 7;
+        BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
+
+        for (Vec3i offset : AROUND_OFFSETS) {
+            blockpos$mutableblockpos.setWithOffset(pos, offset);
+            i = Math.min(i, getDistanceAt(level.getBlockState(blockpos$mutableblockpos)) + 1);
+            if (i == 1) {
+                break;
+            }
+        }
+
+        return state.setValue(DISTANCE, i);
+    }
+
+    private static int getDistanceAt(BlockState neighbor) {
+        return getOptionalDistanceAt(neighbor).orElse(7);
+    }
+
+    public static OptionalInt getOptionalDistanceAt(BlockState state) {
+        if (state.is(BlockTags.LOGS)) {
+            return OptionalInt.of(0);
+        } else {
+            return state.hasProperty(DISTANCE) ? OptionalInt.of(state.getValue(DISTANCE)) : OptionalInt.empty();
+        }
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
+        BlockState blockstate = this.defaultBlockState().setValue(PERSISTENT, true).setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
+        return updateDistance(blockstate, context.getLevel(), context.getClickedPos());
     }
 }
