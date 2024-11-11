e86f23170ee02d6fcd6e0a44109068cd41fcdfa1
Gegy <gegy.dev@gmail.com>
2024-11-11 15:06:56 +0100

    Fix: don't consume coconut item before accessing properties, don't try to predict client-side

--- src/main/java/net/tropicraft/core/common/item/ExplodingCoconutItem.java
+++ src/main/java/net/tropicraft/core/common/item/ExplodingCoconutItem.java
@@ -21,30 +21,30 @@ public class ExplodingCoconutItem extends Item {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
-        boolean canPlayerThrow = player.isCreative() || player.canUseGameMasterBlocks() || TropicsConfigs.COMMON.allowExplodingCoconutsByNonOPs.get();
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack item = player.getItemInHand(hand);
-        if (!canPlayerThrow) {
-            if (!world.isClientSide) {
-                player.displayClientMessage(TropicraftLangKeys.EXPLODING_COCONUT_WARNING.component(), false);
-            }
-            return new InteractionResultHolder<>(InteractionResult.PASS, item);
+        if (level.isClientSide()) {
+            return new InteractionResultHolder<>(InteractionResult.CONSUME, item);
         }
 
-        if (!player.isCreative()) {
-            item.shrink(1);
+        boolean canPlayerThrow = player.isCreative() || player.canUseGameMasterBlocks() || TropicsConfigs.COMMON.allowExplodingCoconutsByNonOPs.get();
+        if (!canPlayerThrow) {
+            player.displayClientMessage(TropicraftLangKeys.EXPLODING_COCONUT_WARNING.component(), false);
+            return new InteractionResultHolder<>(InteractionResult.FAIL, item);
         }
-        world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SNOWBALL_THROW, SoundSource.NEUTRAL, 0.5f, 0.4f / (player.getRandom().nextFloat() * 0.4f + 0.8f));
-        if (!world.isClientSide) {
-            float explosionRadius = item.getOrDefault(TropicraftDataComponents.EXPLOSION_RADIUS, ExplodingCoconutEntity.DEFAULT_EXPLOSION_RADIUS);
-            boolean destroysBlocks = item.getOrDefault(TropicraftDataComponents.DESTROYS_BLOCKS, ExplodingCoconutEntity.DEFAULT_DESTROYS_BLOCKS);
-            ExplodingCoconutEntity coconut = new ExplodingCoconutEntity(world, player, explosionRadius, destroysBlocks);
-            coconut.setItem(item);
-            coconut.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0f, 1.5f, 1.0f);
-            world.addFreshEntity(coconut);
-        }
+
+        float explosionRadius = item.getOrDefault(TropicraftDataComponents.EXPLOSION_RADIUS, ExplodingCoconutEntity.DEFAULT_EXPLOSION_RADIUS);
+        boolean destroysBlocks = item.getOrDefault(TropicraftDataComponents.DESTROYS_BLOCKS, ExplodingCoconutEntity.DEFAULT_DESTROYS_BLOCKS);
+        ExplodingCoconutEntity coconut = new ExplodingCoconutEntity(level, player, explosionRadius, destroysBlocks);
+        coconut.setItem(item);
+        coconut.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0f, 1.5f, 1.0f);
+        level.addFreshEntity(coconut);
+        level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SNOWBALL_THROW, SoundSource.NEUTRAL, 0.5f, 0.4f / (player.getRandom().nextFloat() * 0.4f + 0.8f));
 
         player.awardStat(Stats.ITEM_USED.get(this));
+
+        item.consume(1, player);
+
         return new InteractionResultHolder<>(InteractionResult.SUCCESS, item);
     }
 }
