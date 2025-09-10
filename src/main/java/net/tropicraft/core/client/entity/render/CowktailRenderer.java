541c6159a1149a4aaaa9f518f3a061824fdbfbb9
baguchan <shutux500@gmail.com>
2025-09-10 18:34:21 +0900

    almost entity model fix!

--- src/main/java/net/tropicraft/core/client/entity/render/CowktailRenderer.java
+++ src/main/java/net/tropicraft/core/client/entity/render/CowktailRenderer.java
@@ -1,8 +1,8 @@
 package net.tropicraft.core.client.entity.render;
 
 import net.minecraft.client.model.CowModel;
+import net.minecraft.client.renderer.entity.AgeableMobRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.tropicraft.Tropicraft;
 import net.tropicraft.core.client.TropicraftRenderLayers;
@@ -10,12 +10,12 @@ import net.tropicraft.core.client.entity.render.layer.CowktailLayer;
 import net.tropicraft.core.client.entity.render.state.CowktailRenderState;
 import net.tropicraft.core.common.entity.passive.CowktailEntity;
 
-public class CowktailRenderer extends MobRenderer<CowktailEntity, CowktailRenderState, CowModel> {
+public class CowktailRenderer extends AgeableMobRenderer<CowktailEntity, CowktailRenderState, CowModel> {
     private static final ResourceLocation IRIS_TEXTURE = Tropicraft.location("textures/entity/cowktail/iris_cowktail.png");
     private static final ResourceLocation ANEMONE_TEXTURE = Tropicraft.location("textures/entity/cowktail/anemone_cowktail.png");
 
     public CowktailRenderer(EntityRendererProvider.Context context) {
-        super(context, new CowModel(context.bakeLayer(TropicraftRenderLayers.COWKTAIL_LAYER)), 0.7f);
+        super(context, new CowModel(context.bakeLayer(TropicraftRenderLayers.COWKTAIL_LAYER)), new CowModel(context.bakeLayer(TropicraftRenderLayers.COWKTAIL_BABY_LAYER)), 0.7f);
         addLayer(new CowktailLayer<>(this, context.getBlockRenderDispatcher()));
     }
 
