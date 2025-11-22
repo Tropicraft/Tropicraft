1f4f904c8f6085176aafbaea0d9153153f190f75
Gegy <me@gegy.dev>
2025-11-22 19:28:51 +0100

    FIXY

--- src/main/java/net/tropicraft/core/common/entity/passive/TropiCreeperEntity.java
+++ src/main/java/net/tropicraft/core/common/entity/passive/TropiCreeperEntity.java
@@ -31,6 +31,8 @@ import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Cat;
 import net.minecraft.world.entity.animal.Ocelot;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -65,7 +67,7 @@ public class TropiCreeperEntity extends PathfinderMob {
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return PathfinderMob.createMobAttributes()
+        return Monster.createMonsterAttributes()
                 .add(Attributes.MOVEMENT_SPEED, 0.25);
     }
 
