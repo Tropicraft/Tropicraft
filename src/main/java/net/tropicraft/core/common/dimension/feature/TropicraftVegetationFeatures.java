d36f1dae78b2dd1d21ac51096d6e349d0c156c9e
Tyouto <kruselaszlo@gmail.com>
2025-11-29 21:38:24 +0100

    switching back because wrong

--- src/main/java/net/tropicraft/core/common/dimension/feature/TropicraftVegetationFeatures.java
+++ src/main/java/net/tropicraft/core/common/dimension/feature/TropicraftVegetationFeatures.java
@@ -119,7 +119,7 @@ public final class TropicraftVegetationFeatures {
         register(context, COFFEE_BUSH, TropicraftFeatures.COFFEE_BUSH);
 
         register(context, UNDERGROWTH, TropicraftFeatures.UNDERGROWTH);
-        register(context, SINGLE_UNDERGROWTH, TropicraftFeatures.SINGLE_UNDERGROWTH, new SimpleTreeFeatureConfig(() -> TropicraftBlocks.MAHOGANY_LOG.get().defaultBlockState(), () -> TropicraftBlocks.KAPOK_LEAVES.get().defaultBlockState().setValue(LeavesBlock.DISTANCE, 1)));
+        register(context, SINGLE_UNDERGROWTH, TropicraftFeatures.SINGLE_UNDERGROWTH, new SimpleTreeFeatureConfig(() -> TropicraftBlocks.MAHOGANY_LOG.get().defaultBlockState(), () -> TropicraftBlocks.MAHOGANY_LEAVES.get().defaultBlockState().setValue(LeavesBlock.DISTANCE, 1)));
         register(context, RED_FLOWERING_BUSH, TropicraftFeatures.SINGLE_UNDERGROWTH, new SimpleTreeFeatureConfig(() -> TropicraftBlocks.MAHOGANY_LOG.get().defaultBlockState(), () -> TropicraftBlocks.RED_FLOWERING_LEAVES.get().defaultBlockState().setValue(LeavesBlock.DISTANCE, 1)));
         register(context, WHITE_FLOWERING_BUSH, TropicraftFeatures.SINGLE_UNDERGROWTH, new SimpleTreeFeatureConfig(() -> TropicraftBlocks.MAHOGANY_LOG.get().defaultBlockState(), () -> TropicraftBlocks.WHITE_FLOWERING_LEAVES.get().defaultBlockState().setValue(LeavesBlock.DISTANCE, 1)));
         register(context, BLUE_FLOWERING_BUSH, TropicraftFeatures.SINGLE_UNDERGROWTH, new SimpleTreeFeatureConfig(() -> TropicraftBlocks.MAHOGANY_LOG.get().defaultBlockState(), () -> TropicraftBlocks.BLUE_FLOWERING_LEAVES.get().defaultBlockState().setValue(LeavesBlock.DISTANCE, 1)));
